910f4a5b3330449745166a5204f0b13c55b76ff9
qifu <fuqiqi12345@gmail.com>
2016-03-19 15:34:41 -0400

    test

---
+++ src/leetcode/test.java
@@ -0,0 +1,25 @@
+package leetcode; /**
+ * Created by qifu on 16/1/28.
+ */
+import leetcode.Solution;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+import java.util.Set;
+import java.util.Map.Entry;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.math.BigDecimal;
+
+public class test {
+
+
+    public static void main(String[] args) {
+
+    }
+}
