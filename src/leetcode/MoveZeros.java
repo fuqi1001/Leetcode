0367a0a1b5d7b2b1e6c3a24258bb066473023dea
qifu <fuqiqi12345@gmail.com>
2016-08-18 00:19:11 -0400

    leetcode283

---
+++ src/leetcode/MoveZeros.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/8/18.
+ */
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        for(int i = 0 ; i < nums.length ; i++){
+            int val = nums[i];
+            int zeros = 0;
+            if(val == 0){
+                zeros++;
+            }
+            else{
+                nums[i] = 0;
+                nums[i - zeros] = val;
+            }
+        }
+    }
+
+    public void moveZeroes(int[] nums) {
+        int cnt = 0, pos = 0;
+        // 将非0数字都尽可能向前排
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[pos]= nums[i];
+                pos++;
+            }
+        }
+        // 将剩余的都置0
+        for(;pos<nums.length; pos++){
+            nums[pos] = 0;
+        }
+    }
+}
