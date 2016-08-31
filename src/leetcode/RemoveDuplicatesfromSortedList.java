58ebf30076f292fe2479458f784d21b44ba25f1b
qifu <fuqiqi12345@gmail.com>
2016-08-30 21:04:11 -0400

    leetcode83

---
+++ src/leetcode/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/8/30.
+ */
+public class RemoveDuplicatesfromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+
+        ListNode H = head;
+        while(head.next != null){
+            if(head.val == head.next.val){
+                head.next = head.next.next;
+            }else{
+                head = head.next;
+            }
+        }
+
+        return H;
+    }
+}
