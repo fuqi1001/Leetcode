a1322b41d501c2a30c970c58d90241d61ba1daef
qifu <fuqiqi12345@gmail.com>
2016-09-06 14:27:44 -0400

    leetcode121

---
+++ src/leetcode/BestTimetoBuyandSellStock.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/9/6.
+ */
+public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        int profit = 0;
+        int minValue = Integer.MAX_VALUE;
+        for(int pr : prices){
+            minValue = pr < minValue ? pr : minValue;
+            profit = (pr - minValue) > profit ? (pr - minValue) : profit;
+        }
+        return profit;
+    }
+
+
+}
