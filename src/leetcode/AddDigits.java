fb894e8d8783a8df7f461613b3ae1c76b766c407
qifu <fuqiqi12345@gmail.com>
2016-09-06 22:00:21 -0400

    leetcode258

---
+++ src/leetcode/AddDigits.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/9/6.
+ */
+public class AddDigits {
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        else{
+            return num % 9;
+        }
+    }
+
+    //https://discuss.leetcode.com/topic/52727/2ms-one-line-java-solution/3
+    /*
+    0~9 (10 nums) :      0,1,2,3,4,5,6,7,8,9
+    10~18(9 nums):         1,2,3,4,5,6,7,8,9
+    19~27(9 nums):         1,2,3,4,5,6,7,8,9
+    and so on
+     */
+}
