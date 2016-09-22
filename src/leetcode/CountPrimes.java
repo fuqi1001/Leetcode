8a77acefa97f0cd4e2210c45187d8d44793c43a5
qifu <fuqiqi12345@gmail.com>
2016-09-22 09:23:51 -0400

    leetcode204

---
+++ src/leetcode/CountPrimes.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/9/22.
+ */
+public class CountPrimes {
+    public int countPrimes(int n){
+        boolean[] notPrime = new boolean[n+2];
+        notPrime[0] = notPrime[1] = true;
+        for(int i = 2 ; i * i<n ;i++){
+            if(!notPrime[i]){
+                int c =i * i;
+                while(c < n){
+                    notPrime[c] = true;
+                    c+=i;
+                }
+            }
+        }
+        int result=0;
+        for(int i = 0 ; i < n ;i++){
+            if(!notPrime[i])
+                result++;
+        }
+        return result;
+    }
+}
