e31bc6edac238ce6d532dfa615ba81c6e34bed7f
qifu <fuqiqi12345@gmail.com>
2016-09-29 11:53:49 -0400

    leetcode338

---
+++ src/leetcode/bit/Counting_Bits.java
@@ -0,0 +1,24 @@
+package leetcode.bit;
+
+/**
+ * Created by qifu on 16/9/29.
+ */
+public class Counting_Bits {
+    public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        int pow2 = 1;
+        int before = 1;
+        for(int i =1; i <= num; i++){
+            if(i == pow2){
+                before = res[i] = 1;
+                pow2<<=1;
+            }
+            else{
+                res[i] = res[before] + 1;
+                before+=1;
+            }
+        }
+        return res;
+
+    }
+}
