f37927e86ea2118bad756b32ab7684f018954620
qifu <fuqiqi12345@gmail.com>
2016-10-10 20:09:49 -0400

    leetcode319

---
+++ src/leetcode/BulbSwitcher.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Created by qifu on 16/10/10.
+ */
+public class BulbSwitcher {
+    public int bulbSwitch(int n) {
+        if(n == 0 )return 0;
+        return (int)Math.sqrt(n);
+
+    }
+}
