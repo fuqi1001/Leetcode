3d8623a1bd3e653c3e4a15f70f9cb09ee50b679a
qifu <fuqiqi12345@gmail.com>
2016-10-18 23:11:41 -0400

    leetcode416

---
+++ src/leetcode/DPandBackTracking/PartitionEqualSubsetSum.java
@@ -0,0 +1,25 @@
+package leetcode.DPandBackTracking;
+
+/**
+ * Created by qifu on 16/10/18.
+ */
+public class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for(int i = 0 ; i < nums.length ;i++){
+            for(int j = target; j >= nums[i] ;j--){
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+}
