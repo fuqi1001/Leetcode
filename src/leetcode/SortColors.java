9dc79c40b0d2b2aada8b8f9d23a11e8d5bcbc539
qifu <fuqiqi12345@gmail.com>
2016-10-22 03:05:35 -0400

    leetcode75

--- src/leetcode/SortColors.java
+++ src/leetcode/SortColors.java
@@ -19,4 +19,31 @@ public class SortColors {
 
     }
     //
+    public void sortColors(int[] nums){
+        if(nums.length == 1) return;
+        int left = 0 ;
+        int right = nums.length - 1;
+        int i = 0 ;
+        int count = 0;
+        while(i <= right ){
+            if(nums[i] == 0){
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] =temp;
+                left++;
+                i++;
+            }
+
+            else if(nums[i] == 1){
+                i++;
+            }
+            else if(nums[i] == 2){
+                int temp = nums[right];
+                nums[right] = nums[i];
+                nums[i] = temp;
+                right--;
+            }
+        }
+
+    }
 }
