60c5a75d519383edab45f3f47d0927b5927bef07
qifu <fuqiqi12345@gmail.com>
2016-11-18 15:34:26 -0500

    leetcode332

---
+++ src/ReconstructItinerary.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Created by qifu on 16/11/18.
+ */
+public class ReconstructItinerary {
+    Map<String, PriorityQueue<String>> flights;
+    LinkedList<String> path;
+
+    public List<String> findItinerary(String[][] tickets) {
+        flights = new HashMap<>();
+        path = new LinkedList<>();
+
+        for(String[] ticket : tickets){
+            flights.putIfAbsent(ticket[0], new PriorityQueue<>());
+            flights.get(ticket[0]).add(ticket[1]);
+        }
+        dfs("JFK");
+        return path;
+    }
+
+    private void dfs(String departure){
+        PriorityQueue<String> arrival = flights.get(departure);
+        while(arrival != null && !arrival.isEmpty()){
+            dfs(arrival.poll());
+        }
+        path.addFirst(departure);
+    }
+}
