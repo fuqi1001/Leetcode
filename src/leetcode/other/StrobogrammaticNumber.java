9bd36b4adc1ee790e5062f0fc3e805c63ef815b3
qifu <fuqiqi12345@gmail.com>
2016-12-03 00:50:12 -0500

    leetcode246

---
+++ src/leetcode/other/StrobogrammaticNumber.java
@@ -0,0 +1,29 @@
+package leetcode.other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by qifu on 16/12/3.
+ */
+public class StrobogrammaticNumber {
+    public boolean isStrobogrammatic(String num) {
+        int len = num.length();
+        int start = 0;
+        int end = len - 1;
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put('0','0');
+        map.put('1','1');
+        map.put('6','9');
+        map.put('8','8');
+        map.put('9','6');
+        while(start  <= end){
+            if(! map.containsKey(num.charAt(start)) || num.charAt(end) != map.get(num.charAt(start) )) return false;
+
+            start++;end--;
+        }
+        return true;
+
+    }
+}
