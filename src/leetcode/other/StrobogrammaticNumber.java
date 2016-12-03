49e47c166da8c0f15e1f17d9180df14efa3841ef
qifu <fuqiqi12345@gmail.com>
2016-12-03 00:54:10 -0500

    leetcode246

--- src/leetcode/other/StrobogrammaticNumber.java
+++ src/leetcode/other/StrobogrammaticNumber.java
@@ -26,4 +26,13 @@ public class StrobogrammaticNumber {
         return true;
 
     }
+
+
+    //
+    public boolean isStrobogrammatic(String num) {
+        for(int i = 0 , j = num.length() - 1 ; i <= j ; i++, j--){
+            if(!"00 11 88 696".contains(num.charAt(i) +""+num.charAt(j))) return false;
+        }
+        return true;
+    }
 }
