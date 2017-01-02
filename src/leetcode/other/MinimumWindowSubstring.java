95f659c9903da15e450b21cac674b7d076f17e4f
qifu <fuqiqi12345@gmail.com>
2017-01-02 09:26:00 +0800

    leetcode76

---
+++ src/leetcode/other/MinimumWindowSubstring.java
@@ -0,0 +1,47 @@
+package leetcode.other;
+
+/**
+ * Created by qifu on 17/1/2.
+ */
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        int[] hash = new int[256];
+        for(char c : t.toCharArray()){
+            hash[c]++;
+        }
+        int counter = t.length();
+        int left = 0;
+        int right = 0;
+        int d = Integer.MAX_VALUE;
+        int head = 0;
+
+        while(right < s.length()){
+            /*if(hash[s.charAt(right++)]-- > 0) counter--;
+            while(counter == 0){
+                if(right - left < d) {
+                    d = right - left;
+                    head = right;
+                }
+
+                if(hash[s.charAt(left++)]++ == 0) counter++;
+            }*/
+
+            if(hash[s.charAt(right)] > 0) counter--;
+            hash[s.charAt(right)]--;
+            right++;
+
+            while(counter == 0){
+                if(right - left < d){
+                    head = left;
+                    d = right - left;
+                }
+
+                hash[s.charAt(left)]++;
+                if(hash[s.charAt(left)] > 0) counter++;
+                left++;
+            }
+        }
+
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
+    }
+}
