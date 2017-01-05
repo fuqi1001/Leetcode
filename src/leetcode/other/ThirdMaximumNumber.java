0f4be741e063cc2cbd702e606560218e3cd7e316
qifu <fuqiqi12345@gmail.com>
2017-01-05 15:58:41 +0800

    leetcode414

---
+++ src/leetcode/other/ThirdMaximumNumber.java
@@ -0,0 +1,37 @@
+package leetcode.other;
+
+/**
+ * Created by qifu on 17/1/5.
+ */
+public class ThirdMaximumNumber {
+    public int thirdMax(int[] nums){
+        int max, mid, small;
+
+        int count = 0;
+        max = mid = small = Integer.MIN_VALUE;
+
+        for(int num : nums){
+            if(count > 0 && max == num || count > 1 && mid == num) continue;
+
+            count++;
+
+            if(num > max){
+                small = mid;
+                mid = max;
+                max = num;
+            } else if( num > mid){
+                small = mid;
+                mid = num;
+            } else if( num > small){
+                small = num;
+            }
+
+
+        }
+        if(count >= 3 ){
+            return small;
+        }else {
+            return max;
+        }
+    }
+}
