e107ccc66952fa2a27340893ee56c9132001a3e2
qifu <fuqiqi12345@gmail.com>
2017-02-03 11:53:01 -0500

    leetcode253

--- src/leetcode/other/MeetingRoomsII.java
+++ src/leetcode/other/MeetingRoomsII.java
@@ -35,8 +35,32 @@ public class MeetingRoomsII {
             endTimes.add(intervals[i].end);
         }
         return endTimes.size();
+    }
 
 
+    ////
+    public int minMeetingRooms(Interval[] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
 
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+
+        for(int i = 0 ; i < intervals.length; i++){
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int endIndex = 0;
+        int room = 0;
+        for(int i = 0 ; i < start.length; i++){
+            if(start[i] < end[endIndex]){
+                room++;
+            } else {
+                endIndex++;
+            }
+        }
+        return room;
     }
 }
