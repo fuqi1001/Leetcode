6c8fe40b3cc14b2ffb61b76bfb0f072d839cfe9d
qifu <fuqiqi12345@gmail.com>
2017-02-13 14:06:12 -0500

    leetcode515

---
+++ src/leetcode/Tree/FindLargestValueinEachTreeRow.java
@@ -0,0 +1,28 @@
+package leetcode.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by qifu on 17/2/13.
+ */
+public class FindLargestValueinEachTreeRow {
+    public int[] largestValues(TreeNode root) {
+        if(root==null) return new int[0];
+        List<Integer> res = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int max = q.peek().val;
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+                TreeNode cur = q.poll();
+                max = Math.max(max, cur.val);
+                if(cur.left!=null) q.add(cur.left);
+                if(cur.right!=null) q.add(cur.right);
+            }
+            res.add(max);
+        }
+        return res.stream().mapToInt(k->k).toArray();
+    }
+}
