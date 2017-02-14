0a2304696c792b37ce626d69c4faabcc56514f3b
qifu <fuqiqi12345@gmail.com>
2017-02-14 15:45:52 -0500

    leetcode304

--- src/leetcode/other/RangeSumQuery2D.java
+++ src/leetcode/other/RangeSumQuery2D.java
@@ -13,10 +13,11 @@ public class RangeSumQuery2D {
         int n = matrix[0].length;
 
         dp = new int[m + 1][n + 1];
+
+        //dp start from 1 -> board
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i-1][j-1];
-
             }
         }
     }
