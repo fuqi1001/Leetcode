0efb6ac088c303957faf0e4ecb3377d104d7a0c4
qifu <fuqiqi12345@gmail.com>
2017-02-15 14:30:44 -0500

    leetcode468

--- src/leetcode/other/ValidateIPAddress.java
+++ src/leetcode/other/ValidateIPAddress.java
@@ -5,48 +5,49 @@ package leetcode.other;
  */
 public class ValidateIPAddress {
     public String validIPAddress(String IP) {
-        if (IP.contains(".")) {
-            String[] IPs = IP.split("\\.");
-            if (IPs.length != 4) return "Neither";
-            for (int i = 0; i < 4; i++) {
+        if(IP.contains(".")) {
+            String[] ip4 = IP.split("\\.");
+            if(ip4.length != 4) return "Neither";
+            if(IP.charAt(IP.length() - 1) == '.') return "Neither";
+
+            int index = 0;
+
+            for(String cur : ip4) {
+
                 try {
-                    int digit = Integer.parseInt(IPs[i]);
+                    int digit = Integer.parseInt(cur);
+                    if(digit < 0 || digit > 255) {
+                        //System.out.println("1");
+                        return "Neither";
+                    }
+                    if(!ip4[index++].equals(String.valueOf(digit))){
+                        //System.out.println("2");
+                        return "Neither";
 
-                    if (digit < 0 || digit > 255) return "Neither";
-
-                    if (!IPs[i].equals(String.valueOf(digit))) return "Neither";
-
-                } catch (NumberFormatException ignored) {
+                    }
+                } catch(NumberFormatException ignored){
                     return "Neither";
                 }
             }
             return "IPv4";
-        } else if (IP.contains(":")) {
-            String[] IPss = IP.split(":");
-            if (IPss.length != 8) return "Neither";
 
-            if (IPss.length == 0) return "Neither";
-            for (int i = 0; i < 8; i++) {
-                String temp = IPss[i];
-                if (temp.length() < 1 || temp.length() > 4) return "Neither";
+        } else if(IP.contains(":")){
+            String[] ip6 = IP.split(":");
+            if(ip6.length != 8) return "Neither";
+            if(IP.charAt(IP.length() - 1) == ':') return "Neither";
 
-                for (char c : temp.toCharArray()) {
-                    if (!isHex(c)) {
-                        System.out.println(c);
-                        return "Neither";
+            for(String str : ip6) {
+                if(str.length() < 1 || str.length() > 4) return "Neither";
 
-                    }
+                for(int i = 0; i < str.length(); i++){
+                    if(!isHex(str.charAt(i))) return "Neither";
                 }
             }
             return "IPv6";
-        } else {
-            return "Neither";
         }
-
-
+        return "Neither";
     }
-
-    public boolean isHex(char c) {
-        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+    private boolean isHex(char c){
+        return (c >= '0' && c <='9') || ( c >= 'a' && c <= 'f' ) ||(c >= 'A' && c <='F') ;
     }
 }
