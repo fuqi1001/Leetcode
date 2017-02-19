1f7069a56701b9c3696abd88a517a272539720af
qifu <fuqiqi12345@gmail.com>
2017-02-19 10:04:32 -0500

    leetcode283

--- src/leetcode/other/MoveZeros.java
+++ src/leetcode/other/MoveZeros.java
@@ -32,4 +32,25 @@ public class MoveZeros {
             nums[pos] = 0;
         }
     }
+
+
+    //暴力
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        int find = start;
+        while(start < nums.length){
+            if(nums[start] == 0){
+                while(find < nums.length - 1 &&nums[find] == 0 ) find++;
+                if(nums[find] != 0){
+                    int temp = nums[find];
+                    nums[find] = nums[start];
+                    nums[start] = temp;
+                }
+            }
+            if(nums[start] == 0 && find >= nums.length - 1) break;
+            start++;
+            find = start;
+
+        }
+    }
 }
