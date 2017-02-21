a35a0e9672fb2f5d6c8199e425b929b4e94d8dae
qifu <fuqiqi12345@gmail.com>
2017-02-21 15:05:22 -0500

    leetcode2

--- src/leetcode/other/AddTwoNumbers.java
+++ src/leetcode/other/AddTwoNumbers.java
@@ -5,68 +5,54 @@ package leetcode.other;
  */
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode newHead = new ListNode(0);
-        ListNode dummy = new ListNode(0);
-        newHead.next = dummy;
-        int temp = 0;
-        while(l1 != null && l2 != null){
-            if(l1.val + l2.val + temp>= 10){
-                l1.val = (l1.val + l2.val + temp) % 10;
-                dummy.next = l1;
-                dummy = dummy.next;
-                l1 = l1.next;
-                l2 = l2.next;
-                temp = 1;
-            }else{
-                l1.val = (l1.val + l2.val + temp) % 10;
-                dummy.next = l1;
-                if(l1.val >= 10){
-                    temp = 1;
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode newHead = new ListNode(0);
+            ListNode cur = new ListNode(0);
+            newHead.next = cur;
+
+            int carry = 0;
+
+            while(l1 != null && l2 != null){
+                int sum = l1.val + l2.val + carry;
+                //System.out.println(sum);
+                if(sum / 10 >= 1) {
+                    carry = 1;
+                }else {
+                    carry = 0;
                 }
-                else {
-                    temp = 0;
+                sum %= 10;
+                cur.next = new ListNode(sum);
+                cur = cur.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            while(l1 != null){
+                int sum = l1.val + carry;
+                if(sum / 10 >= 1){
+                    carry = 1;
+                } else {
+                    carry = 0;
                 }
-                dummy = dummy.next;
-                l1 = l1.next;
-                l2 = l2.next;
-
-            }
-        }
-        while(l1 != null){
-            if(temp == 1){
-                l1.val =  l1.val + temp;
-                if(l1.val < 10) temp = 0;
-                l1.val = l1.val % 10;
-                dummy.next = l1 ;
-                dummy = dummy.next;
-                l1 = l1.next;
-
-            }else{
-                dummy.next = l1;
-                dummy = dummy.next;
+                sum %= 10;
+                cur.next = new ListNode(sum);
+                cur = cur.next;
                 l1 = l1.next;
             }
-        }
-        while(l2 != null){
-            if(temp == 1){
-                l2.val =  (l2.val + temp) ;
-                if(l2.val < 10) temp = 0;
-                l2.val =l2.val % 10;
-                dummy.next = l2 ;
-
-                dummy = dummy.next;
-                l2 = l2.next;
-
-            }else{
-                dummy.next = l2;
-                dummy = dummy.next;
+            while(l2 != null){
+                int sum = l2.val + carry;
+                if(sum / 10 >= 1){
+                    carry = 1;
+                }else {
+                    carry = 0;
+                }
+                sum %= 10;
+                cur.next = new ListNode(sum);
+                cur = cur.next;
                 l2 = l2.next;
             }
-        }
-        if(temp == 1){
-            dummy.next = new ListNode(1);
-        }
+            if(carry == 1) cur.next = new ListNode(1);
+            return newHead.next.next;
 
-        return newHead.next.next;
-    }
+        }
 }
