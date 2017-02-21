697212708e17abfc2be3e13ef2cfb2d8b3efe365
qifu <fuqiqi12345@gmail.com>
2017-02-21 10:05:56 -0500

    leetcode101

--- src/leetcode/Tree/SymmetricTree.java
+++ src/leetcode/Tree/SymmetricTree.java
@@ -53,7 +53,7 @@ public class SymmetricTree {
         if(left == null && right == null)
             return true;
         if(left == null || right == null)
-            return false;
+            return left == right;
 
         return (left.val == right.val) && isSymmetricTree(left.left,right.right) && isSymmetricTree(left.right,right.left);
     }
