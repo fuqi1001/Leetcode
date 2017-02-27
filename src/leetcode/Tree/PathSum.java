44b2ba7e2f414d1bde267b5e069ef29e9e2971de
qifu <fuqiqi12345@gmail.com>
2017-02-27 11:34:51 -0500

    leetcode112

--- src/leetcode/Tree/PathSum.java
+++ src/leetcode/Tree/PathSum.java
@@ -1,5 +1,7 @@
 package leetcode.Tree;
 
+import java.util.Stack;
+
 /**
  * Created by qifu on 16/9/15.
  */
@@ -18,5 +20,38 @@ public class PathSum {
 
         return hasPathSum(node.left,sum,target) || hasPathSum(node.right,sum,target);
 
+    }
+
+    //
+    public boolean hasPathSum(TreeNode root, int sum){
+        if(root == null) return false;
+        if(root.left == null && root.right == null){
+            return (root.val == sum);
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+
+    //
+    public boolean hasPathSum(TreeNode root, int sum){
+        if(root == null) return false;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty() && root != null){
+            TreeNode cur = stack.pop();
+            if(cur.left == null && cur.right == null){
+                if(cur.val == sum) return true;
+            }
+
+            if(cur.right != null){
+                cur.right.val = cur.val + cur.right.val;
+                stack.push(cur.right);
+            }
+            if(cur.left != null){
+                cur.left.val = cur.val + cur.left.val;
+                stack.push(cur.left);
+            }
+        }
+        return false;
     }
 }
