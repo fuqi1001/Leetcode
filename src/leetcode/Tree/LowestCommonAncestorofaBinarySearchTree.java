6bdf2013bda8fcdd8fdd3d34e2033266955b00c9
qifu <fuqiqi12345@gmail.com>
2017-02-27 13:57:39 -0500

    leetcode235

--- src/leetcode/Tree/LowestCommonAncestorofaBinarySearchTree.java
+++ src/leetcode/Tree/LowestCommonAncestorofaBinarySearchTree.java
@@ -10,5 +10,31 @@ public class LowestCommonAncestorofaBinarySearchTree {
             if(p.val < root.val) root = root.left;
             else root = root.right;
         }
+
+    }
+
+    //
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+    //
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int min = Math.min(p.val, q.val);
+        int max = Math.max(p.val, q.val);
+        while(true){
+            if(root.val < min){
+                root = root.right;
+            } else if(root.val > max){
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
     }
 }
