9836b6611d6da8f4a6a0c2fec2f8f4742ef7652e
qifu <fuqiqi12345@gmail.com>
2017-02-27 22:24:00 -0500

    leetcode107

--- src/leetcode/Tree/BinaryTreeLevelOrderTraversalII.java
+++ src/leetcode/Tree/BinaryTreeLevelOrderTraversalII.java
@@ -41,4 +41,40 @@ public class BinaryTreeLevelOrderTraversalII {
         result.pop();
         return result;
     }
+
+    //
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return res;
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> cur = new ArrayList<>();
+            while(size-- > 0){
+                TreeNode node = queue.poll();
+                cur.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            res.add(0, cur);
+        }
+        return res;
+    }
+
+    //
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        helper(res, root, 0);
+        return res;
+    }
+    public void helper(List<List<Integer>> res, TreeNode root, int height){
+        if(root == null) return;
+        if(height >= res.size()){
+            res.add(0, new LinkedList<>());
+        }
+        helper(res, root.left, height + 1);
+        helper(res, root.right, height + 1);
+        res.get(res.size() - height - 1).add(root.val);
+    }
 }
