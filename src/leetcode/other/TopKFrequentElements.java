bc2508eb9803067ebe4d8eaee954fe817fdc854b
qifu <fuqiqi12345@gmail.com>
2017-03-02 11:31:26 -0500

    leetcode347

--- src/leetcode/other/TopKFrequentElements.java
+++ src/leetcode/other/TopKFrequentElements.java
@@ -85,4 +85,28 @@ public class TopKFrequentElements {
     最后从maxCount 开始往回扫
     知道result里面存了 >= k 个对象为止
      */
+
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
+        for(int num : map.keySet()) {
+            int freq = map.get(num);
+            if(!treeMap.containsKey(freq)) {
+                treeMap.put(freq, new ArrayList<>());
+            }
+            treeMap.get(freq).add(num);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while(res.size() < k) {
+            Map.Entry<Integer, List<Integer>> entry = treeMap.pollLastEntry();
+            res.addAll(entry.getValue());
+        }
+        return res;
+    }
 }
