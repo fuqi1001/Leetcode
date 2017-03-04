739e7bd9e463327548b87ad66cf7ccd7a5d2d15e
qifu <fuqiqi12345@gmail.com>
2017-03-04 14:46:56 -0500

    leetcode366

--- src/leetcode/Tree/FindLeavesofBinaryTree.java
+++ src/leetcode/Tree/FindLeavesofBinaryTree.java
@@ -40,4 +40,20 @@ public class FindLeavesofBinaryTree {
 
         return false;
     }
+
+    //
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        height(root, res);
+        return res;
+    }
+    private int height(TreeNode root, List<List<Integer>> res) {
+        if(root == null) return -1;
+        int level = 1 + Math.max(height(root.left, res), height(root.right, res));
+        if(res.size() < level + 1) {
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        return level;
+    }
 }
