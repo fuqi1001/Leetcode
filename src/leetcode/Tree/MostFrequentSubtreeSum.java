2625fce4050565312d3e2711e223c86874dd5f24
qifu <fuqiqi12345@gmail.com>
2017-03-05 11:53:22 -0500

    leetcode508

--- src/leetcode/Tree/MostFrequentSubtreeSum.java
+++ src/leetcode/Tree/MostFrequentSubtreeSum.java
@@ -1,37 +1,59 @@
 package leetcode.Tree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qifu on 17/2/4.
  */
 public class MostFrequentSubtreeSum {
+    //
+    private int max = 0;
     public int[] findFrequentTreeSum(TreeNode root) {
         if(root == null) return new int[0];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        helper(root, map);
+        if(root.left == null && root.right == null){
+            int[] res = new int[1];
+            res[0] = root.val;
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int temp = Sum(root, map);
+        List<Integer> list = new ArrayList<>();
 
-        System.out.println(map.keySet());
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            //System.out.println(entry.getValue());
+            if(entry.getValue() == max) {
+                list.add(entry.getKey());
+            }
+        }
 
-        int[] res = new int[1];
+        int[] res = new int[list.size()];
+        int index = 0;
+        for(int num : list) {
+            res[index++] = num;
+        }
         return res;
     }
 
-    public int helper(TreeNode root, HashMap<Integer,Integer> map){
-        int res = root.val;
-        if(root.left != null){
-            res += helper(root.left, map);
-        }
-        if(root.right != null){
-            res += helper(root.right, map);
-        }
-
-        if(map.containsKey(res)){
-            map.put(res,map.get(res) + 1);
-        } else {
-            map.put(res, 1);
+    public int Sum(TreeNode root, Map<Integer, Integer> map) {
+        // System.out.println("run");
+        if(root == null) return 0;
+        if(root.left == null  && root.right == null ){
+            map.put(root.val, map.getOrDefault(root.val, 0) + 1);
+            max = Math.max(max, map.get(root.val));
+            return root.val;
         }
 
-        return res;
+        int left = Sum(root.left, map);
+        int right = Sum(root.right, map);
+
+        int cur = root.val + left + right;
+
+        map.put(cur, map.getOrDefault(cur, 0) + 1);
+        max = Math.max(max, map.get(cur));
+        //System.out.println("max" +max);
+        return cur;
     }
 }
