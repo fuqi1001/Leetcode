5eb99b89702dd68b2a2fb3df01e202365b32c349
qifu <fuqiqi12345@gmail.com>
2017-03-04 21:05:17 -0500

    leetcode98

--- src/leetcode/Tree/ValidateBinarySearchTree.java
+++ src/leetcode/Tree/ValidateBinarySearchTree.java
@@ -14,4 +14,15 @@ public class ValidateBinarySearchTree {
                 && helper(cur.left, low, cur.val)
                 && helper(cur.right, cur.val, high);
     }
+
+    //
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+    public boolean helper(TreeNode root, Integer min, Integer max) {
+        if(root == null) return true;
+        if(min != null && min >= root.val) return false;
+        if(max != null && max <= root.val) return false;
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
 }
