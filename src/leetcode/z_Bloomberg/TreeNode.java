8a44704071eac14af7aa15dc424a49838879f6c4
qifu <fuqiqi12345@gmail.com>
2017-03-06 14:11:18 -0500

    TreeNode

---
+++ src/leetcode/z_Bloomberg/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode.z_Bloomberg;
+
+/**
+ * Created by qifu on 17/3/6.
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
