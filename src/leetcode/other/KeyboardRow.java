bde86b857d1daf5b610ba2a7a4adc5da6f46d642
qifu <fuqiqi12345@gmail.com>
2017-03-09 11:14:45 -0500

    leetcode500

--- src/leetcode/other/KeyboardRow.java
+++ src/leetcode/other/KeyboardRow.java
@@ -53,4 +53,35 @@ public class KeyboardRow {
         }
         return res;
     }
+
+    //
+    public String[] findWords(String[] words) {
+        String[] strs = {"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            for(int j = 0; j < strs[i].length(); j++){
+                map.put(strs[i].charAt(j), i);
+            }
+        }
+        List<String> list = new ArrayList<>();
+
+        for(String cur : words) {
+            if(cur == "") continue;
+            String temp = cur.toUpperCase();
+            int index = map.get(temp.charAt(0));
+            for(int i = 1; i < temp.length(); i++) {
+                if(map.get(temp.charAt(i)) != index) {
+                    index = -1;
+                    break;
+                }
+            }
+            if(index != -1) list.add(cur);
+        }
+        String[] res = new String[list.size()];
+        int index = 0;
+        for(String cur : list){
+            res[index++] = cur;
+        }
+        return res;
+    }
 }
