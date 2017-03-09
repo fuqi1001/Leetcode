d65bf3d80cfa02b7d30efe97b4d0f5f4f3371df2
qifu <fuqiqi12345@gmail.com>
2017-03-09 12:26:30 -0500

    leetcode412

--- src/leetcode/other/FizzBuzz.java
+++ src/leetcode/other/FizzBuzz.java
@@ -22,4 +22,30 @@ public class FizzBuzz {
         }
         return res;
     }
+
+    //follow up
+    public List<String> fizzBuzz(int n) {
+        int fizz = 0;
+        int buzz = 0;
+        List<String> res = new ArrayList<>();
+
+        for(int i = 1; i <= n ; i++) {
+            fizz++;
+            buzz++;
+            if(fizz == 3 && buzz == 5) {
+                fizz = 0;
+                buzz = 0;
+                res.add("FizzBuzz");
+            } else if(fizz == 3) {
+                fizz = 0;
+                res.add("Fizz");
+            } else if(buzz == 5) {
+                buzz = 0;
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
 }
