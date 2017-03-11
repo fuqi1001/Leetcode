2a49dc1c037be0d9c93a20cabd7d44d4b4fb2c62
qifu <fuqiqi12345@gmail.com>
2017-03-11 15:45:44 -0500

    leetcode

--- src/leetcode/other/ShortestWordDistance.java
+++ src/leetcode/other/ShortestWordDistance.java
@@ -1,5 +1,10 @@
 package leetcode.other;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by qifu on 16/12/1.
  */
@@ -17,5 +22,28 @@ public class ShortestWordDistance {
             }
         }
         return minDis;
+    }
+        ////////
+    public int shortestDistance(String[] words, String word1, String word2) {
+        Map<String, List<Integer>> map = new HashMap<>();
+        int index = 0;
+        for(String word : words) {
+            if(map.containsKey(word)) {
+                map.get(word).add(index++);
+            } else {
+                map.put(word, new LinkedList<>());
+                map.get(word).add(index++);
+            }
+        }
+        List<Integer> list1 = map.get(word1);
+        List<Integer> list2 = map.get(word2);
+
+        int min = Integer.MAX_VALUE;
+        for(int i : list1) {
+            for(int j : list2) {
+                min = Math.min(min, Math.abs(i-j));
+            }
+        }
+        return min;
     }
 }
