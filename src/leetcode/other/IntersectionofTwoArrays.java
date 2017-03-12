a2a9b2b820bb960e193bf77d88dbe3004d999963
qifu <fuqiqi12345@gmail.com>
2017-03-12 15:38:27 -0400

    leetcode349

--- src/leetcode/other/IntersectionofTwoArrays.java
+++ src/leetcode/other/IntersectionofTwoArrays.java
@@ -2,6 +2,7 @@ package leetcode.other;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by qifu on 16/8/18.
@@ -65,4 +66,42 @@ public class IntersectionofTwoArrays {
         return result;
 
     }
+
+    //binary search
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            /*if (binarySearch(nums2, num)) {
+                set.add(num);
+            }*/
+            if(Arrays.binarySearch(nums2, num) >= 0) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    /*public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }*/
 }
