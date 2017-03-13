ce1ee32be37c020ac6459b9afa2bdf93466674be
qifu <fuqiqi12345@gmail.com>
2017-03-13 14:13:52 -0400

    leetcode

--- src/leetcode/other/NumberofBoomerangs.java
+++ src/leetcode/other/NumberofBoomerangs.java
@@ -32,4 +32,25 @@ public class NumberofBoomerangs {
     private int getDistance(int[] a, int[] b){
         return (a[0] - b[0])*(a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
     }
+
+    //
+    public int numberOfBoomerangs(int[][] points) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for(int[] point1 : points) {
+            for(int[] point2 : points) {
+                if(point1 == point2) continue;
+                int dist = (point1[0] - point2[0]) * (point1[0] - point2[0]) + (point1[1] - point2[1]) *(point1[1] - point2[1]);
+                int prev = map.containsKey(dist) ? map.get(dist) : 0;
+                res += 2 * prev;
+                map.put(dist, prev + 1);
+            }
+            map.clear():
+        }
+        return res;
+    }
+    /*
+    * 2 * prev
+    *
+    * */
 }
