460040daa78338de52bd1355e7d51e8fa1ce5c3e
qifu <fuqiqi12345@gmail.com>
2017-03-14 12:38:49 -0400

    leetcode438

--- src/leetcode/other/FindAllAnagramsinaString.java
+++ src/leetcode/other/FindAllAnagramsinaString.java
@@ -7,22 +7,35 @@ import java.util.List;
  * Created by qifu on 16/12/31.
  */
 public class FindAllAnagramsinaString {
-    public List<Integer> findAnagrams(String s, String p){
-        List<Integer> res = new ArrayList<>();
-        if(s == null || s.length() == 0 || p == null || p.length() == 0) return res;
-        int left = 0;
-        int right= 0;
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
         int[] hash = new int[256];
-        for(char c: p.toCharArray()){
-            hash[c]++;
+        if(s == null || p == null || s.length() == 0|| p.length() == 0) return list;
+        for(int i = 0; i < p.length(); i++){
+            hash[p.charAt(i)]++;
         }
-
+        int left = 0;
+        int right = 0;
         int count = p.length();
-        while(right < s.length()){
-            if(hash[s.charAt(right++)]-- >= 1) count--;
-            if(count == 0) res.add(left);
-            if(right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+
+        while(right < s.length()) {
+            if(hash[s.charAt(right)] >= 1) {
+                count--;
+            }
+            hash[s.charAt(right)]--;
+            right++;
+
+            if(count == 0) list.add(left);
+
+            if(right - left == p.length()){
+                if(hash[s.charAt(left)] >= 0){
+                    count++;
+                }
+                hash[s.charAt(left)]++;
+                left++;
+            }
+
         }
-        return res;
+        return list;
     }
 }
