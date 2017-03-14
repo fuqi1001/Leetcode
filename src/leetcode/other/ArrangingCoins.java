9227a99facffed4f6bab72aa9d7f4328431b3772
qifu <fuqiqi12345@gmail.com>
2017-03-14 09:58:39 -0400

    leetcode

--- src/leetcode/other/ArrangingCoins.java
+++ src/leetcode/other/ArrangingCoins.java
@@ -8,26 +8,19 @@ public class ArrangingCoins {
         return (int) ((Math.sqrt(8 * (long)n + 1) - 1) * 0.5);
     }
 
-
+    //
     public int arrangeCoins(int n) {
-        long nLong = (long)n;
-
-        long start = 0;
-        long end = (n / 2) + 1;
-
-        long mid = 0;
-
-        while(start <= end){
-            mid = start + (end - start) / 2;
-
-
-            if(mid * (mid + 1) <= 2 * nLong){
-                start = mid + 1;
-            }else{
-                end = mid - 1;
+        int left = 0;
+        int right = n;
+        int mid = 0;
+        while(left <= right) {
+            mid = (left + right) >>> 1;
+            if(0.5 * mid * mid + 0.5 * mid <= n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
-        return (int)(start - 1);
+        return left - 1;
     }
 }
