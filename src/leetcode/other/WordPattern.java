6ddcd1879c6029a10f024bcf0f56457e4e6dccf8
qifu <fuqiqi12345@gmail.com>
2017-03-14 14:03:57 -0400

    leetcode290

--- src/leetcode/other/WordPattern.java
+++ src/leetcode/other/WordPattern.java
@@ -16,6 +16,27 @@ public class WordPattern {
             if(map.put(word[i],i) != map.put(pattern.charAt(i),i)) return false;
         }
         return true;
+
+        //map.put() returns the previous value associated with key,
+        //or null if there was no mapping for key.
+
+
+        //map.containsValue();
+        public boolean wordPattern(String pattern, String str) {
+            String[] words = str.split(" ");
+            Map<Character, String> map = new HashMap<>();
+            if(words.length != pattern.length()) return false;
+            for(int i = 0; i < pattern.length(); i++){
+                char cur = pattern.charAt(i);
+                if(map.containsKey(cur)){
+                    if(!map.get(cur).equals(words[i])) return false;
+                } else {
+                    if(map.containsValue(words[i])) return false;
+                    map.put(cur, words[i]);
+                }
+            }
+            return true;
+        }
     }
 
 }
