4c1c9391188c2299583226de4c2ebaca63d8219e
qifu <fuqiqi12345@gmail.com>
2017-03-14 10:32:29 -0400

    leetcode9

--- src/leetcode/other/PalindromeNumber.java
+++ src/leetcode/other/PalindromeNumber.java
@@ -5,20 +5,32 @@ package leetcode.other;
  */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if(x < 0 ) return false;
+        if(x < 0 ||(x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while(x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return x == rev || x == rev/10;
+    }
+
+    //
+    public boolean isPalindrome(int x) {
+        if( x < 0) return false;
+
         int div = 1;
-        while(x / div > 10){
+        while(x / div >= 10){
             div *= 10;
         }
+        //System.out.println("***"+div);
+
         while(x > 0){
+            //System.out.println("***"+div);
             int left = x / div;
             int right = x % 10;
-
             if(left != right) return false;
-
             x = x % div / 10;
             div /= 100;
-        }
-        return true;
+        }return true;
     }
 }
