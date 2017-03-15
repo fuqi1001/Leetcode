92ad4027d53dff91089242d7ee7492a3803222d1
qifu <fuqiqi12345@gmail.com>
2017-03-15 16:50:41 -0400

    leetcode125

--- src/leetcode/other/ValidPalindrome.java
+++ src/leetcode/other/ValidPalindrome.java
@@ -27,4 +27,36 @@ public class ValidPalindrome {
         }
         return true;
     }
+
+    //
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0) return true;
+        s = s.toLowerCase();
+        int left = 0;
+        int right =s.length() - 1;
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            char c1 = s.charAt(left);
+            char c2 = s.charAt(right);
+            System.out.println(c1 +" "  +c2);
+            if(c1 != c2) return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    //
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
 }
