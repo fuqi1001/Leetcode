ddb769759f8876b9a3845865b468e73ff6e97ce2
qifu <fuqiqi12345@gmail.com>
2017-03-16 14:07:53 -0400

    leetcode362

--- src/leetcode/Implement/DesignHitCounter.java
+++ src/leetcode/Implement/DesignHitCounter.java
@@ -1,5 +1,8 @@
 package leetcode.Implement;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by qifu on 17/1/7.
  */
@@ -36,6 +39,28 @@ public class DesignHitCounter {
             }
         }
         return total;
+    }
+
+    //queue
+    Queue<Integer> queue;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        queue = new LinkedList<Integer>();
+    }
+
+    /** Record a hit.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        queue.add(timestamp);
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while(!queue.isEmpty() && timestamp - queue.peek() >= 300) {
+            queue.poll();
+        }
+        return queue.size();
     }
 }
 
