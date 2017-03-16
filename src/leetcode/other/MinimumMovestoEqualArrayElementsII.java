454eadb8f2b73016a5ae69d0ff26a7b53be16531
qifu <fuqiqi12345@gmail.com>
2017-03-16 17:51:49 -0400

    leetcode462

--- src/leetcode/other/MinimumMovestoEqualArrayElementsII.java
+++ src/leetcode/other/MinimumMovestoEqualArrayElementsII.java
@@ -36,4 +36,31 @@ public class MinimumMovestoEqualArrayElementsII {
         Arrays.sort(res);
         return res[0];
     }
+    //
+    /*
+    Suppose you have two endpoints A and B,
+    when you want to find a point C that has minimum sum of distance between AC and BC,
+    the point C will always between A and B. Draw a graph and you will understand it.
+    Lets keep moving forward. After we locating the point C between A and B,
+    we can define that
+    dis(AC) = c - a; dis(CB) = b - c;
+    sum = dis(AC) + dis(CB) = b - a.
+    b - a will be a constant value, given specific b and a.
+    Thus there will be no difference between points among A and B.
+
+    In this problem, we set two boundaries, saying i and j,
+    and we move the i and j to do the computation.
+     */
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+        int count = 0;
+        while(start < end) {
+            count += nums[end] - nums[start];
+            start++;
+            end--;
+        }
+        return count;
+    }
 }
