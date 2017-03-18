670531fe6b5d2014d4206ba085b4d2e945a410dd
qifu <fuqiqi12345@gmail.com>
2017-03-18 19:41:46 -0400

    leetcode311

--- src/leetcode/other/SparseMatrixMultiplication.java
+++ src/leetcode/other/SparseMatrixMultiplication.java
@@ -4,18 +4,19 @@ package leetcode.other;
  * Created by qifu on 17/1/10.
  */
 public class SparseMatrixMultiplication {
-    public int[][] multiply(int[][] A, int[][] B){
+    public int[][] multiply(int[][] A, int[][] B) {
         int m = A.length;
         int n = A[0].length;
         int nB = B[0].length;
+
         int[][] C = new int[m][nB];
-        for(int i = 0 ; i < m; i++){
-            for(int j = 0 ; j < n; j++){
-                if(A[i][j] != 0){
-                    for(int k = 0 ; k < nB; k++){
-                        if(B[j][k] != 0) C[i][k] += A[i][j] * B[j][k];
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < n; k++) {
+                if(A[i][k] != 0)
+                    for(int j = 0; j < nB; j++) {
+                        if(B[k][j] != 0)
+                            C[i][j] += A[i][k] * B[k][j];
                     }
-                }
             }
         }
         return C;
