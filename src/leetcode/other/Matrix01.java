093c15c4a041293413bd91db2068705a450669e9
qifu <fuqiqi12345@gmail.com>
2017-03-20 12:29:16 -0400

    leetcode542

--- src/leetcode/other/Matrix01.java
+++ src/leetcode/other/Matrix01.java
@@ -45,71 +45,55 @@ public class Matrix01 {
     }
 
 
-    ///dp way, but wrong
+    ///dp way
     public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(matrix.size() == 0) return res;
         int m = matrix.size();
         int n = matrix.get(0).size();
+
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++) {
-                if(matrix.get(i).get(j) == 1) {
-                    if(check(matrix, i ,j)) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = Integer.MAX_VALUE;
-                    }
+        for(int[] cur : dp) {
+            Arrays.fill(cur, Integer.MAX_VALUE);
+        }
+        for(int i =  0; i < m;  i++) {
+            for(int j = 0;  j < n; j++) {
+                if(matrix.get(i).get(j) == 0) dp[i][j] = 0;
+                else if(dp[i][j] != 0) {
+                    if(i > 0 && dp[i-1][j] != Integer.MAX_VALUE && dp[i][j] > dp[i-1][j] + 1)
+                        dp[i][j] = dp[i-1][j] + 1;
+                    if(j > 0 && dp[i][j-1] != Integer.MAX_VALUE && dp[i][j] > dp[i][j-1] + 1)
+                        dp[i][j] = dp[i][j-1] + 1;
+                    if(i < m - 1 && dp[i+1][j] != Integer.MAX_VALUE && dp[i][j] > dp[i+1][j] + 1)
+                        dp[i][j] = dp[i+1][j] + 1;
+                    if(j < n - 1 && dp[i][j+1] != Integer.MAX_VALUE && dp[i][j] > dp[i][j+1] + 1)
+                        dp[i][j] = dp[i][j+1] + 1;
                 }
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(dp[i][j] == Integer.MAX_VALUE) {
-                    dp[i][j] = plus(dp, i,j);
+
+        for(int i = m - 1; i >= 0 ;  i-- ) {
+            for(int j = n - 1 ;  j >= 0 ;  j-- ) {
+                if(matrix.get(i).get(j) == 0) dp[i][j] = 0;
+                else if(dp[i][j] != 0) {
+                    if(i > 0 && dp[i-1][j] != Integer.MAX_VALUE && dp[i][j] > dp[i-1][j] + 1)
+                        dp[i][j] = dp[i-1][j] + 1;
+                    if(j > 0 && dp[i][j-1] != Integer.MAX_VALUE && dp[i][j] > dp[i][j-1] + 1)
+                        dp[i][j] = dp[i][j-1] + 1;
+                    if(i < m - 1 && dp[i+1][j] != Integer.MAX_VALUE && dp[i][j] > dp[i+1][j] + 1)
+                        dp[i][j] = dp[i+1][j] + 1;
+                    if(j < n - 1 && dp[i][j+1] != Integer.MAX_VALUE && dp[i][j] > dp[i][j+1] + 1)
+                        dp[i][j] = dp[i][j+1] + 1;
                 }
             }
         }
-        for(int i = 0; i < m; i++) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int[] row : dp) {
             List<Integer> list = new ArrayList<>();
-            for(int j = 0; j < n; j++){
-                list.add(dp[i][j]);
+            for(int num : row) {
+                list.add(num);
             }
             res.add(new ArrayList<>(list));
         }
         return res;
-    }
-
-    public boolean check(List<List<Integer>> matrix, int i ,int j) {
-        boolean flag = false;
-        if(i + 1 < matrix.size() && matrix.get(i + 1).get(j) == 0) flag = true;
-        else if(i - 1 >= 0 && matrix.get(i - 1).get(j) == 0) flag = true;
-        else if(j + 1 < matrix.get(0).size() && matrix.get(i).get(j + 1) == 0) flag = true;
-        else if(j - 1 >= 0 && matrix.get(i).get(j - 1) == 0) flag = true;
-
-        return flag;
-    }
-    public int plus(int[][] dp, int i , int j) {
-        int up = Integer.MAX_VALUE;
-        int down = Integer.MAX_VALUE;
-        int left = Integer.MAX_VALUE;
-        int right = Integer.MAX_VALUE;
-        if(i - 1 >= 0 && dp[i-1][j] != Integer.MAX_VALUE) {
-            up = dp[i-1][j] + 1;
-        }
-        if(j - 1 >= 0 && dp[i][j - 1] != Integer.MAX_VALUE) {
-            left = dp[i][j-1] + 1;
-        }
-        if(i + 1 < dp.length && dp[i+1][j] != Integer.MAX_VALUE) {
-            down = dp[i+1][j] + 1;
-        }
-        if(j + 1 < dp[0].length && dp[i][j+1] != Integer.MAX_VALUE) {
-            right = dp[i][j+1] + 1;
-        }
-        if(i == 0 && j == 8) {
-            System.out.println(up+" "+down+" "+left +" "+right);
-        }
-        return Math.min(Math.min(up, down), Math.min(right , left));
     }
 
 }
