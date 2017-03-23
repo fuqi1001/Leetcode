188ecffc407e6686e4d93f6b671ad59adce8c616
qifu <fuqiqi12345@gmail.com>
2017-03-23 02:40:43 -0400

    leetcode416

--- src/leetcode/DPandBackTracking/PartitionEqualSubsetSum.java
+++ src/leetcode/DPandBackTracking/PartitionEqualSubsetSum.java
@@ -6,20 +6,38 @@ package leetcode.DPandBackTracking;
 public class PartitionEqualSubsetSum {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int num : nums){
+        for(int num : nums) {
             sum += num;
         }
-        if(sum % 2 != 0) return false;
-
-        int target = sum / 2;
-        boolean[] dp = new boolean[target + 1];
+        if((sum & 1) == 1) return false;
+        sum /= 2;
+        boolean[] dp = new boolean[sum + 1];
         dp[0] = true;
-
-        for(int i = 0 ; i < nums.length ;i++){
-            for(int j = target; j >= nums[i] ;j--){
-                dp[j] = dp[j] || dp[j - nums[i]];
+        for(int num : nums) {
+            for(int i = sum; i > 0 ; i--) {
+                if(i >= num)
+                    dp[i] = dp[i] || dp[i - num];
             }
         }
-        return dp[target];
+        return dp[sum];
     }
+
 }
+/*
+
+
+    part 1:
+    分成两组相同, 说明总和可以被2除尽
+
+
+    part 2:
+    01背包
+    dp 用来存是否存在和为i 的组合
+    dp[0] = true;
+
+    因为 i = i - n + n;
+    所以 dp[i] 是否为true 可又  i - n 推出 ( i - n + n )
+    dp[i] = dp[i] || dp[i - n]
+
+    最后返回dp[sum]
+ */
