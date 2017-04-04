9eeb2297956a4c3d2f5ef1f71aa21cefbabd9f6f
qifu <fuqiqi12345@gmail.com>
2017-04-04 13:45:59 -0400

    leetcode247

--- src/leetcode/other/StrobogrammaticNumberII.java
+++ src/leetcode/other/StrobogrammaticNumberII.java
@@ -7,46 +7,46 @@ import java.util.List;
  * Created by qifu on 17/2/3.
  */
 public class StrobogrammaticNumberII {
-    List<String> res = new ArrayList<>();
+    List<String> res;
     public List<String> findStrobogrammatic(int n) {
-        helper(new char[n],0,n - 1 );
+        res = new ArrayList<>();
+        helper(new char[n], 0, n-1);
         return res;
     }
-    private void helper(char[] a, int left, int right){
-        if(left > right){
-            res.add(new String(a));
+    private void helper(char[] cur, int left, int right) {
+        if(left > right) {
+            res.add(new String(cur));
             return;
         }
-        if(left == right){
-            a[left] = '0';
-            res.add(new String(a));
-            a[left] = '1';
-            res.add(new String(a));
-            a[left] = '8';
-            res.add(new String(a));
+        if(left == right) {
+            cur[left] = '0';
+            res.add(new String(cur));
+            cur[left] = '1';
+            res.add(new String(cur));
+            cur[left] = '8';
+            res.add(new String(cur));
             return;
         }
 
-        if(left != 0){
-            a[left] = '0';
-            a[right] = '0';
-            helper(a, left + 1, right - 1);
+        if(left != 0) {
+            cur[left] = '0';
+            cur[right] = '0';
+            helper(cur, left+1,right-1);
         }
+        cur[left] = '1';
+        cur[right] = '1';
+        helper(cur, left+1,right-1);
 
-        a[left] = '1';
-        a[right] = '1';
-        helper(a, left + 1, right - 1);
+        cur[left] = '6';
+        cur[right] = '9';
+        helper(cur, left+1,right-1);
 
-        a[left] = '8';
-        a[right] = '8';
-        helper(a, left + 1, right - 1);
+        cur[left] = '8';
+        cur[right] = '8';
+        helper(cur, left+1,right-1);
 
-        a[left] = '6';
-        a[right] = '9';
-        helper(a, left + 1, right - 1);
-
-        a[left] = '9';
-        a[right] = '6';
-        helper(a, left + 1, right - 1);
+        cur[left] = '9';
+        cur[right] = '6';
+        helper(cur, left+1,right-1);
     }
 }
