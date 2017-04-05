e244354d5f61e37ac0f42aca3cffc2e80c173036
qifu <fuqiqi12345@gmail.com>
2017-04-05 16:45:13 -0400

    leetcode74

--- src/leetcode/other/Searcha2DMatrix.java
+++ src/leetcode/other/Searcha2DMatrix.java
@@ -4,6 +4,32 @@ package leetcode.other;
  * Created by qifu on 16/10/19.
  */
 public class Searcha2DMatrix {
+
+
+    //
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+        if(matrix[0] == null || matrix[0].length == 0) return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int start = 0, end = row * col - 1;
+        while(start + 1< end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid/col][mid%col];
+            if(number == target) return true;
+            if(number < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if(matrix[start/col][start%col] == target) return true;
+        if(matrix[end/col][end%col] == target) return true;
+        return false;
+    }
+
+    //
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0) return false;
         int row = matrix.length;
@@ -44,39 +70,6 @@ public class Searcha2DMatrix {
 
         if(matrix[start][left] == target) return true;
         else if(matrix[start][right] == target) return true;
-        return false;
-    }
-
-
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) {
-            return false;
-        }
-        if (matrix[0] == null || matrix[0].length == 0) {
-            return false;
-        }
-
-        int row = matrix.length, column = matrix[0].length;
-        int start = 0, end = row * column - 1;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            int number = matrix[mid / column][mid % column];
-            if (number == target) {
-                return true;
-            } else if (number < target) {
-                start = mid;
-            } else {
-                end = mid;
-            }
-        }
-
-        if (matrix[start / column][start % column] == target) {
-            return true;
-        } else if (matrix[end / column][end % column] == target) {
-            return true;
-        }
-
         return false;
     }
 }
