83e25594461e606567a6e8ef320d630c462b70e4
qifu <fuqiqi12345@gmail.com>
2017-04-06 15:07:44 -0400

    leetcode300

--- src/leetcode/other/LongestIncreasingSubsequence.java
+++ src/leetcode/other/LongestIncreasingSubsequence.java
@@ -7,56 +7,41 @@ public class LongestIncreasingSubsequence {
 
     //O(n^2)
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int max = 0;
-        for(int i = 0 ; i < nums.length ;i++){
+        int len = nums.length;
+        int[] dp = new int[len];
+        int res = 0;
+        for(int i = 0; i < len; i++) {
             dp[i] = 1;
-            for(int j = 0 ; j < i ;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = dp[i] > dp[j]+1 ? dp[i] : dp[j] + 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            if(dp[i] > max){
-                max = dp[i];
-            }
+            res = Math.max(dp[i], res);
         }
-        return max;
+        return res;
     }
 
 
     //O(logn)
     //find the first number > num
-    private int binarySearch(int[] minLast, int num){
-        int start = 0 , end = minLast.length - 1;
-        while(start + 1 < end){
-            int mid = start + (end - start) / 2;
-            if(minLast[mid] < num){
-                start = mid;
-            }else{
-                end = mid;
+    public int lengthOfLIS(int[] nums) {
+        int[] tail = new int[nums.length];
+        int size = 0;
+        for(int num : nums) {
+            int i = 0, j = size;
+            while(i != j) {
+                int mid = i + (j - i) / 2;
+                if(tail[mid] < num) {
+                    i = mid + 1;
+                } else {
+                    j = mid;
+                }
             }
+            tail[i] = num;
+            if(i == size) size++;
         }
-
-        if(minLast[start] > num){
-            return start;
-        }
-        return end;
-    }
-
-    public int lengthOfLIS(int[] nums){
-        int[] minLast = new int[nums.length + 1];
-        minLast[0] = -1;
-        for(int i = 1 ; i <= nums.length ;i++){
-            minLast[i] = Integer.MAX_VALUE;
-        }
-        for(int i = 0 ; i < nums.length; i++){
-            int index = binarySearch(minLast,nums[i]);
-            minLast[index] = nums[i];
-        }
-        for(int i = minLast.length - 1 ; i >= 1 ; i--){
-            if(minLast[i] != Integer.MAX_VALUE) return i;
-        }
-        return 0;
+        return size;
     }
 
 
