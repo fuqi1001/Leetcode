35ff568df1bb32580d52418588d9b015eb4a357d
qifu <fuqiqi12345@gmail.com>
2017-04-07 13:12:37 -0400

    leetcode289

--- src/leetcode/other/GameofLife.java
+++ src/leetcode/other/GameofLife.java
@@ -4,63 +4,104 @@ package leetcode.other;
  * Created by qifu on 16/10/24.
  */
 public class GameofLife {
-    public void gameOfLife(int[][] board) {
-        if(board == null || board.length ==0) return;
 
+    //in place
+    /*
+    用两位bits来存
+    00 -> 表示原来是死的,操作完后也是死的
+    01 -> 表示原来是活的,操作完后是死的
+    11 -> 表示原来是获得,操作完后也是活的
+    10 -> 表示原来是死的,操作完后变成活的
+    第一位表示原来,第二位表示现在
+     */
+
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length == 0 || board[0].length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int count = countN(board, i,j);
+
+                if(board[i][j] == 1) {
+                    if(count >= 2 && count <= 3) {
+                        board[i][j] = 3;  //11
+                    }
+                }
+                else if(board[i][j] == 0){
+                    if(count == 3) {
+                        board[i][j] = 2;
+                    }
+                }
+
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                board[i][j] = board[i][j] >> 1;
+            }
+        }
+    }
+    private int countN(int[][] board, int x, int y) {
+        int count = 0;
+        int m = board.length;
+        int n = board[0].length;
+
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(i == 0 && j == 0) continue;
+                if(x + i < 0 || x + i >= m || y + j < 0 || y + j >= n) continue;
+                if( (board[x + i][y + j] & 1 )== 1) count++;
+            }
+        }
+        return count;
+    }
+
+    //
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length == 0 || board[0].length == 0) return;
         int m = board.length;
         int n = board[0].length;
 
         int[][] temp = new int[m][n];
 
-
-        for(int i = 0 ; i < m ;i++){
-            for(int j = 0 ; j < n ;j++){
-                temp[i][j] = 0;
-                int count = countNeighbors(board,i,j);
-                if(board[i][j] == 1){
-                    if(count == 2 || count == 3) {
-                        System.out.println("x: "+i +" y: "+j + " run 1 ");
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int count = countN(board, i,j);
+                //System.out.println(count);
+                if(board[i][j] == 1) {
+                    if(count < 2 || count > 3) {
+                        temp[i][j] = 0;
+                    } else {
                         temp[i][j] = 1;
                     }
-                    else if(count > 3) {
-                        System.out.println("x: "+i +" y: "+j +" run 2" );
-                        temp[i][j] = 0;
-                    }
-                    else{
-                        System.out.println("x: "+i +" y: "+j +" run 3");
-                        temp[i][j] = 0;
-                    }
-                }else if(board[i][j] == 0){
-
+                } else if(board[i][j] == 0){
                     if(count == 3) {
-                        System.out.println("x: "+i +" y: "+j +" run 4");
                         temp[i][j] = 1;
                     }
                 }
             }
         }
-        for(int i = 0 ; i < m ;i++){
-            for(int j = 0 ; j < n ;j++){
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 board[i][j] = temp[i][j];
             }
         }
-
     }
-    public int countNeighbors(int[][] board, int x,int y){
+    private int countN(int[][] board, int x, int y) {
         int count = 0;
         int m = board.length;
         int n = board[0].length;
-        int[] dx = {1,1,1,0,0,-1,-1,-1};
-        int[] dy = {1,0,-1,1,-1,1,0,-1};
-        for(int i = 0 ; i < 8 ;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx < 0 || ny < 0 || nx >=m || ny>=n) continue;
-            if(board[nx][ny] == 1) {
-                count++;
+
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(i == 0 && j == 0) continue;
+                if(x + i < 0 || x + i >= m || y + j < 0 || y + j >= n) continue;
+                if(board[x + i][y + j] == 1) count++;
             }
         }
         return count;
-
     }
 }
