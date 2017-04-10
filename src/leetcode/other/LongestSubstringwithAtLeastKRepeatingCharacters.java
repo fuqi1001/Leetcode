f95ec56f2f7398f04b3ba9a0e781ae670c9ec020
fuqi1001 <fuqiqi12345@gmail.com>
2017-04-09 21:17:48 -0400

    leetcode395

--- src/leetcode/other/LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ src/leetcode/other/LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -5,27 +5,26 @@ package leetcode.other;
  */
 public class LongestSubstringwithAtLeastKRepeatingCharacters {
     public int longestSubstring(String s, int k) {
-        int res = 0;
-        int i = 0;
-        int n = s.length();
-
-        while(i + k < n){
-            int[] map = new int[26];
-            int mask = 0;
-            int max_index = i;
-            for(int j = i ; j < n ;j++){
-                int t = s.charAt(j) - 'a';
-                map[t]++;
-                if(map[t] < k) mask |= (1 << t);
-                else mask &= (~(1<<t));
-                if(mask == 0){
-                    res = Math.max(res,j - i + 1);
-                    max_index = j;
+        return helper(s.toCharArray(), 0, s.length(), k);
+    }
+    public int helper(char[] cur, int start, int end, int k) {
+        if(end - start < k) return 0;
+        int[] count = new int[26];
+        for(int i = start; i < end; i++) {
+            count[cur[i] - 'a']++;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(count[i]  < k) {
+                for(int j = start; j < end; j++) {
+                    if(cur[j] == i+'a') {
+                        int left = helper(cur, start, j, k);
+                        int right = helper(cur, j + 1, end, k);
+                        return Math.max(left, right);
+                    }
                 }
             }
-            i = max_index + 1;
         }
-        return res;
+        return end - start;
     }
 
     //http://www.cnblogs.com/grandyang/p/5852352.html
