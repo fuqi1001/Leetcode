4a5ec266825f0093f92f80d1cf5e4d6127bec503
qifu <fuqiqi12345@gmail.com>
2017-04-11 11:16:21 -0400

    leetcode299

--- src/leetcode/other/BullsandCows.java
+++ src/leetcode/other/BullsandCows.java
@@ -1,5 +1,8 @@
 package leetcode.other;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by qifu on 16/9/16.
  */
@@ -21,5 +24,30 @@ public class BullsandCows {
             }
         }
         return a+"A"+b+"B";
+    }
+
+    //hashmap way
+    
+    public String getHint(String secret, String guess) {
+        int a = 0;
+        int b = 0;
+        int len = secret.length();
+        Map<Character, Integer> mapa = new HashMap<>();
+        Map<Character, Integer> mapb = new HashMap<>();
+        for(int i = 0; i < len; i++) {
+            char ca = secret.charAt(i);
+            char cb = guess.charAt(i);
+            mapa.put(ca, mapa.getOrDefault(ca, 0) + 1);
+            mapb.put(cb, mapb.getOrDefault(cb, 0) + 1);
+            if(ca == cb) a++;
+        }
+        for(char key : mapa.keySet()) {
+            if(mapb.containsKey(key)) {
+                b += Math.min(mapa.get(key), mapb.get(key));
+            }
+        }
+        b -= a;
+        String res = a+"A"+b+"B";
+        return res;
     }
 }
