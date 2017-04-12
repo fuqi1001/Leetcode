520259bdbc401d19d33b5f35c79c9df4c978f130
qifu <fuqiqi12345@gmail.com>
2017-04-12 12:10:42 -0400

    leetcode417

--- src/leetcode/other/PacificAtlanticWaterFlow_bfs.java
+++ src/leetcode/other/PacificAtlanticWaterFlow_bfs.java
@@ -15,9 +15,10 @@ public class PacificAtlanticWaterFlow_bfs {
             {0, 1},
             {0, -1}
     };
+
     public List<int[]> pacificAtlantic(int[][] matrix) {
         List<int[]> res = new ArrayList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
         int m = matrix.length;
         int n = matrix[0].length;
 
@@ -27,13 +28,13 @@ public class PacificAtlanticWaterFlow_bfs {
         Queue<int[]> queue_p = new LinkedList<>();
         Queue<int[]> queue_a = new LinkedList<>();
 
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             queue_p.add(new int[]{i, 0});
             queue_a.add(new int[]{i, n - 1});
             p[i][0] = true;
-            a[i][n-1] = true;
+            a[i][n - 1] = true;
         }
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             queue_p.add(new int[]{0, i});
             queue_a.add(new int[]{m - 1, i});
             p[0][i] = true;
@@ -42,26 +43,29 @@ public class PacificAtlanticWaterFlow_bfs {
         bfs(matrix, queue_p, p);
         bfs(matrix, queue_a, a);
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(p[i][j] && a[i][j]) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (p[i][j] && a[i][j]) {
                     res.add(new int[]{i, j});
                 }
             }
         }
         return res;
     }
+
     private void bfs(int[][] matrix, Queue<int[]> queue, boolean[][] visited) {
         int m = matrix.length;
         int n = matrix[0].length;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int[] node = queue.poll();
-            for(int[] cur : dir) {
+            for (int[] cur : dir) {
                 int x = node[0] + cur[0];
                 int y = node[1] + cur[1];
-                if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] < matrix[node[0]][node[1]] || visited[x][y]) continue;
+                if (x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] < matrix[node[0]][node[1]] || visited[x][y])
+                    continue;
                 visited[x][y] = true;
-                queue.add(new int[] {x,y});
+                queue.add(new int[]{x, y});
             }
         }
     }
+}
