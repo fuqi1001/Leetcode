b418bd8b85c4f7a92921294698dc126ad20d4bfe
qifu <fuqiqi12345@gmail.com>
2017-04-12 12:11:19 -0400

    leetcode417

--- src/leetcode/other/PacificAtlanticWaterFlow_dfs.java
+++ src/leetcode/other/PacificAtlanticWaterFlow_dfs.java
@@ -1,5 +1,6 @@
 package leetcode.other;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,36 +8,33 @@ import java.util.List;
  * Created by qifu on 17/2/6.
  */
 public class PacificAtlanticWaterFlow_dfs {
-
-    int[][] dir = new int[][] {
-            {1,0},
-            {-1,0},
-            {0,1},
-            {0,-1}
+    int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
     };
     public List<int[]> pacificAtlantic(int[][] matrix) {
-        List<int[]> res = new LinkedList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        List<int[]> res = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0] == null) return res;
 
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
+        boolean[][] p = new boolean[m][n];
+        boolean[][] a = new boolean[m][n];
 
-        boolean[][] p = new boolean[n][m];
-        boolean[][] a = new boolean[n][m];
-
-        for(int i = 0 ; i < n ; i++){
-            dfs(matrix,p,Integer.MIN_VALUE,i,0);
-            dfs(matrix,a,Integer.MIN_VALUE,i,m-1);
+        for(int i = 0; i < m; i++) {
+            dfs(matrix, p, Integer.MIN_VALUE, i, 0);
+            dfs(matrix, a, Integer.MIN_VALUE, i, n - 1);
+        }
+        for(int i = 0; i < n; i++) {
+            dfs(matrix, p, Integer.MIN_VALUE, 0, i);
+            dfs(matrix, a, Integer.MIN_VALUE, m - 1, i);
         }
 
-        for(int i = 0; i < m; i++){
-            dfs(matrix,p,Integer.MIN_VALUE,0,i);
-            dfs(matrix,a,Integer.MIN_VALUE,n-1,i);
-        }
-
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < m; j++){
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(p[i][j] && a[i][j]) {
                     res.add(new int[]{i,j});
                 }
@@ -44,19 +42,15 @@ public class PacificAtlanticWaterFlow_dfs {
         }
         return res;
     }
-
-    private void dfs(int[][] matrix, boolean[][] visited, int height, int x, int y){
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        if(x < 0 || x >= n || y < 0 || y >= m || visited[x][y] || matrix[x][y] < height){
-            return;
-        }
+    private void dfs(int[][] matrix, boolean[][] visited, int height, int x, int y) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if(x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || matrix[x][y] < height) return;
 
         visited[x][y] = true;
 
-        for(int[] d: dir){
-            dfs(matrix, visited, matrix[x][y], x + d[0], y + d[1]);
+        for(int[] cur : dir) {
+            dfs(matrix, visited, matrix[x][y], x + cur[0], y + cur[1]);
         }
     }
 }
