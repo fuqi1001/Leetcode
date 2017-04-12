6c95be976bba9e56ba5ff852b1d86ec2c1437966
qifu <fuqiqi12345@gmail.com>
2017-04-12 14:48:05 -0400

    leetcode33

--- src/leetcode/other/SearchInRotatedSortedArray.java
+++ src/leetcode/other/SearchInRotatedSortedArray.java
@@ -16,28 +16,23 @@ package leetcode.other;
 
  */
 public class SearchInRotatedSortedArray {
-    public int search(int[] nums, int target){
-        int len = nums.length;
-        int left = 0;
-        int right = nums.length - 1;
-        while(left <= right){
-            int mid = (left + right) / 2;
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target) return mid;
 
-            if(nums[mid] >= nums[left]){
-                if(nums[left] <= target && target < nums[mid]){
+            if(nums[mid] < nums[right]) {
+                if(nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
                     right = mid - 1;
                 }
-                else{
-                    left = mid + 1;
-                }
-            }
-            else{
-                if(nums[mid] < target && target <= nums[right]){
-                    left = mid + 1;
-                }
-                else{
+            } else {
+                if(nums[left] <= target && nums[mid] > target) {
                     right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
         }
