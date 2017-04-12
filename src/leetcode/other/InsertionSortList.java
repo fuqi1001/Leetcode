e5767282e31eddf4dba8f9d25e96725a71a9a10a
qifu <fuqiqi12345@gmail.com>
2017-04-12 16:30:26 -0400

    leetcode147

--- src/leetcode/other/InsertionSortList.java
+++ src/leetcode/other/InsertionSortList.java
@@ -27,30 +27,32 @@ public class InsertionSortList {
     }
     // 8ms
     public ListNode insertionSortList(ListNode head) {
-        if(head == null) return null;
+        if(head == null) return head;
 
-        ListNode newHead = new ListNode(0);
-        newHead.next = head;
-        ListNode tail = head;
-        ListNode cur = null;
-        ListNode point = head;
+        ListNode dummy = new ListNode(0);
+        ListNode move = head;
+        dummy.next = head;
+        ListNode point = dummy;
 
-        while(tail.next != null){
-            if(tail.val > tail.next.val){
-                cur = tail.next;
-                point = newHead;
-                while(point.next.val <= cur.val){
+        while(move != null ){
+            if(move.next != null && move.val > move.next.val) {
+                ListNode cur = move.next;
+                point = dummy;
+                while(point.next != null && point.next.val < cur.val) {
                     point = point.next;
                 }
-
                 ListNode temp = cur.next;
                 cur.next = point.next;
                 point.next = cur;
-                tail.next =temp;
-            }else{
-                tail = tail.next;
+                move.next = temp;
+            } else {
+                move = move.next;
             }
         }
-        return newHead.next;
+        return dummy.next;
     }
+    /*
+    如果碰到是递增顺序的暂时先不管,开始往下扫
+
+     */
 }
