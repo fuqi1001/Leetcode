0790f7034cc36f1bf758006ef76ec0bc5e26962c
qifu <fuqiqi12345@gmail.com>
2017-04-13 16:00:21 -0400

    leetcode467

--- src/leetcode/other/UniqueSubstringsinWraparoundString.java
+++ src/leetcode/other/UniqueSubstringsinWraparoundString.java
@@ -5,27 +5,32 @@ package leetcode.other;
  */
 public class UniqueSubstringsinWraparoundString {
     public int findSubstringInWraproundString(String p) {
-        int[] maxLen = new int[26];
-        char[] c = p.toCharArray();
         int len = p.length();
-
-        int[] letterLen = new int[len];
-
-        for(int i = 0 ; i < len; i++){
+        int[] map = new int[len];
+        int[] count = new int[26];
+        char[] ctr = p.toCharArray();
+        for(int i = 0; i < len; i++) {
             int curLen = 1;
-            if(0 < i && (c[i] == c[i-1] + 1 || (c[i-1] =='z' && c[i] == 'a'))){
-                curLen = letterLen[i - 1] + 1;
+            if(i > 0 &&(ctr[i-1] + 1 == ctr[i] || (ctr[i-1] =='z' && ctr[i] == 'a'))) {
+                curLen = map[i - 1] + 1;
             }
-            letterLen[i] = curLen;
-            int id = c[i] - 'a';
-            maxLen[id] = Math.max(maxLen[id], curLen);
-
+            map[i] = curLen;
+            int index = ctr[i] - 'a';
+            count[index] = Math.max(count[index], curLen);
         }
         int result = 0;
-        //子串长度就是 子串能出现的排列数
-        for(int i =0 ; i < 26 ; i++){
-            result += maxLen[i];
+        for(int num : count) {
+            result += num;
         }
         return result;
+
+        /*
+        分别用两个数组来储存中间的计算结果
+        map储存当前字符是否可以符合条件组成子串,并记录到目前为止的最长长度
+
+        count用来记录 以当前字符 c[i]结尾的子串的长度,
+        长度就是组合数,然后把长度求和
+        就得到Output
+         */
     }
 }
