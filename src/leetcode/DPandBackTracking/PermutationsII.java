4cc00eec994e99cd58c60cbe4a2c006bc8e3353c
qifu <fuqiqi12345@gmail.com>
2017-04-13 12:31:19 -0400

    leetcode47

--- src/leetcode/DPandBackTracking/PermutationsII.java
+++ src/leetcode/DPandBackTracking/PermutationsII.java
@@ -11,38 +11,27 @@ import java.util.List;
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
+        if(nums == null || nums.length == 0) return res;
+        List<Integer> cur = new ArrayList<>();
+        Arrays.sort(nums);
         int[] visited = new int[nums.length];
-        if(nums == null) return res;
-        if(nums.length == 0){
-            res.add(new ArrayList<>());
-            return res;
-        }
-
-        Arrays.sort(nums); // important
-
-
-        for(int i = 0 ; i < nums.length ;i++){
-            visited[i] = 0 ;
-        }
-        helper(res,list,nums,visited);
+        helper(res, cur, nums, visited);
         return res;
     }
-    public void helper(List<List<Integer>> res,List<Integer> list,int[] nums,int[] visited){
-        if(list.size() == nums.length){
-            res.add(new ArrayList<Integer>(list));
+
+    public void helper(List<List<Integer>> res, List<Integer> cur, int[] nums, int[] visited) {
+        if(cur.size() == nums.length) {
+            res.add(new ArrayList(cur));
             return;
         }
-        for(int i = 0 ;i < nums.length ;i++){
-            if(visited[i] == 1 || (i != 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0)){
-                continue;
-            }
-            visited[i] = 1;
-            list.add(nums[i]);
-            helper(res,list,nums,visited);
-            list.remove(list.size() - 1);
-            visited[i] = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i] == 1 || (i!=0 &&nums[i] == nums[i-1] && visited[i-1] == 1)) continue;
 
+            visited[i] = 1;
+            cur.add(nums[i]);
+            helper(res, cur, nums, visited);
+            cur.remove(cur.size() - 1);
+            visited[i] = 0;
         }
     }
 }
