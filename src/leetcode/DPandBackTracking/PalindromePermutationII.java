188149449ab968c61fda6d2c14fd96c20b9477a7
qifu <fuqiqi12345@gmail.com>
2017-04-13 14:45:06 -0400

    leetcode267

--- src/leetcode/DPandBackTracking/PalindromePermutationII.java
+++ src/leetcode/DPandBackTracking/PalindromePermutationII.java
@@ -10,57 +10,46 @@ import java.util.Map;
  */
 public class PalindromePermutationII {
     public List<String> generatePalindromes(String s) {
+        List<String> res = new ArrayList<>();
+        if(s == null || s.length() == 0) return res;
+        Map<Character, Integer> map = new HashMap<>();
         int odd = 0;
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            odd += map.get(c) % 2 != 0 ? 1 : -1;
+        }
+        if(odd > 1) return res;
         String mid = "";
         List<Character> list = new ArrayList<>();
-        List<String> res = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-
-        // step 1. build character count map and count odds
-        for(int i = 0 ; i < s.length(); i++){
-            char cur = s.charAt(i);
-            map.put(cur, map.containsKey(cur) ? map.get(cur) + 1 : 1);
-            odd += map.get(cur) % 2 != 0 ? 1 : -1;
-        }
-        // cannot form any palindromic string
-        if(odd > 1) return res;
-
-
-
-
-        // step 2. add half count of each character to list
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
             char key = entry.getKey();
-            int val = entry.getValue();
+            int value = entry.getValue();
 
-            if(val % 2 != 0) mid += key;
-            for(int i = 0 ; i < val / 2; i++){
+            if(value % 2 != 0) mid += key;
+            for(int i = 0; i < value / 2; i++) {
                 list.add(key);
             }
         }
-        // step 3. generate all the permutations
-        helper(list, mid, new boolean[list.size()], new StringBuilder(), res);
+        boolean[] visited = new boolean[list.size()];
+        StringBuilder sb = new StringBuilder();
+        helper(res, list, visited, sb, mid);
         return res;
     }
 
-    private void helper(List<Character> list, String mid, boolean[] used, StringBuilder sb, List<String> res){
-        if(sb.length() == list.size()){
-            String curRes = sb.toString() + mid + sb.reverse().toString();
-            res.add(curRes);
+    private void helper(List<String> res, List<Character> list, boolean[] visited, StringBuilder sb, String mid) {
+        if(sb.length() == list.size()) {
+            String str = sb.toString() + mid + sb.reverse().toString();
+            res.add(str);
             sb.reverse();
             return;
         }
-        for(int i = 0; i < list.size(); i++){
-            // avoid duplication
-            if(i > 0 && list.get(i) == list.get(i - 1) && !used[i - 1]) continue;
-
-            if(!used[i]){
-                used[i] = true;
+        for(int i = 0; i < list.size(); i++) {
+            if(i > 0 && list.get(i) == list.get(i - 1) && visited[i - 1] == false) continue;
+            if(visited[i] == false) {
+                visited[i] = true;
                 sb.append(list.get(i));
-                //recursion
-                helper(list,mid,used,sb,res);
-                used[i] = false;
-                //backtracking
+                helper(res, list, visited, sb, mid);
+                visited[i] = false;
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
