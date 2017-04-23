92405054756c3e2e26214e15f90e71cff0449965
qifu <fuqiqi12345@gmail.com>
2017-04-22 23:56:55 -0400

    logic problem

---
+++ src/leetcode/z_IXL/logicProblem.java
@@ -0,0 +1,35 @@
+package leetcode.z_IXL;
+
+/**
+ * Created by qifu on 17/4/22.
+ */
+public class logicProblem {
+    /*
+    1. Alex said Bob break the glass
+    2. Bob said Dane break the glass
+    3. Chris said not him
+    4. Dane said Bob is lie
+
+
+    if only one lie:
+    Bob is lying
+
+    1 true
+    2 fail
+    3 true
+    4 true
+    Bob break the glass
+
+    if only one true
+    Dane is true
+
+    1 false, so bob not break the glass
+    2 false, so Dane not break the glass
+    3 false, so Chris break the glass
+    4 true, so Dane not break the glass
+
+    so Chris break the glass
+
+
+     */
+}
