714cd6da699d31c70b8cca06cfedc154433943a7
qifu <fuqiqi12345@gmail.com>
2017-04-24 15:24:06 -0400

    leetcode

--- src/leetcode/z_IXL/kthElementFromListEnd.java
+++ src/leetcode/z_IXL/kthElementFromListEnd.java
@@ -9,6 +9,10 @@ import leetcode.Tree.ListNode;
 public class kthElementFromListEnd {
 
     //O(n)
+    /*
+    count the length of the list
+    then go through it, util the kth from the end
+     */
     public int find(ListNode root, int k) {
         int len = 0;
         ListNode temp = root;
@@ -27,7 +31,13 @@ public class kthElementFromListEnd {
     }
 
     //O(n) two point
-
+    /*
+    use fast and slow two point,
+    fast point move first, after k step,
+    begin to move fast and slow at the same time
+    when the fast point get the end
+    slow point is exactly the kth element from the end
+     */
     public int find(ListNode root, int k) {
         ListNode fast = root;
         ListNode slow = root;
