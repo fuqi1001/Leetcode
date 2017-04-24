c4b6a126c2dfcb90beba7eac54379772a27bcfaf
qifu <fuqiqi12345@gmail.com>
2017-04-24 15:41:41 -0400

    leetcode

--- src/leetcode/z_IXL/KthLargestElement.java
+++ src/leetcode/z_IXL/KthLargestElement.java
@@ -34,5 +34,14 @@ public class KthLargestElement {
     找这个数组的范围
     以每个数字与最小值的差为index 保存每个number出现的次数
     然后倒序找
+    first, we go through the array to find the range of this array
+    then go through again the array, count the element minus minimal element in the array
+    use the different as the index, keep the frequency of a element show up in an array name count
+    At the end, we go through the count array from the end.
+    Because the end of the count always the larges element in the origin array
+    keep k - frequencey of current element , until k less or equal 0
+    the use the index + minimal we can get the original element
+    which is the kth largest element
+
      */
 }
