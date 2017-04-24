18b827af6989f5600c89d22b79dbc1b2f573c575
qifu <fuqiqi12345@gmail.com>
2017-04-24 15:15:09 -0400

    leetcode

--- src/leetcode/z_IXL/implementQueUseStack.java
+++ src/leetcode/z_IXL/implementQueUseStack.java
@@ -33,4 +33,19 @@ public class implementQueUseStack {
     public boolean empty() {
         return input.isEmpty() && output.isEmpty();
     }
+
+    /*
+    I have one input stack, for pushing incoming elements,
+    and using one output stack, from which I peek/pop
+    I move the elements from input stack to output stack when needed
+    when I need to peek or pop but the output stack is empty,
+    I will move all elements from input to output stack, thereby reversing the order
+    so that will be correct order for peek/pop function for a queue
+
+    The loop in peek does the moving from input to output stack.
+    Each element only ever gets moved like that once, though, and only after we already
+    spent time pushing it, so overall amortized cose for each operation is O(1)
+
+
+     */
 }
