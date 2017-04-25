ce02cf3b67faa441cfbce992010984e137a0f44e
qifu <fuqiqi12345@gmail.com>
2017-04-24 22:29:43 -0400

    leetcode

--- src/leetcode/z_IXL/twoSum.java
+++ src/leetcode/z_IXL/twoSum.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
  * Created by qifu on 17/4/21.
  */
 public class twoSum {
+
+    //O(n)
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,6 +21,45 @@ public class twoSum {
         }
         return res;
     }
+    /*
+    for this problem, we just need to go through the array one time,
+    and use a hashMap to keep the current num value and the index
+    what we need to do just use map to find appropriate value to make it be target
+     */
 
+
+    //do without hashmap
+
+    public int[] twoSum(int[] nums, int target) {
+        int max = nums[0];
+        int min = nums[0];
+
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] < min){
+                min = nums[i];
+            }
+        }
+
+        int Min =  Math.min(min,target - max);
+        int Max =  Math.max(max,target - min);
+
+        Integer[] map = new Integer[Max - Min + 1];
+        target = target - Min;
+        for(int i = 0 ; i < nums.length ;i++){
+            int key = target - nums[i];
+            if(map[key] != null){
+                return new int[]{map[key] , i};
+            }
+            else{
+                map[nums[i] - Min] = i;
+            }
+        }
+        return new int[]{-1,-1};
+    }
 
 }
