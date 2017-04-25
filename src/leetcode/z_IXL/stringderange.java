a692228dd5deb09b841f29249b3f4237646c8b13
qifu <fuqiqi12345@gmail.com>
2017-04-25 11:51:48 -0400

    leetcode

--- src/leetcode/z_IXL/stringderange.java
+++ src/leetcode/z_IXL/stringderange.java
@@ -21,7 +21,6 @@ public class stringderange {
 
     public static void derangement( char[] origin, boolean[] used, StringBuilder sb, List<String> result) {
         if (sb.length() == origin.length ) {
-            //System.out.println(sb.toString());
             result.add(sb.toString());
         }
         for (int i = 0; i < origin.length; i++) {
@@ -33,4 +32,31 @@ public class stringderange {
             used[i] = false;
         }
     }
+
+    /*
+    use a backtrack or dfs way to recursive solve this problem,
+    what we need to do is generate all the permutation of the string
+    but omit the string which has the character in origin place
+    First, we cover the string to a char array,
+    and create a boolean array to check the character be visited or not,
+    create a StringBuilder object to keep the current string we get in each recursive
+    and a result list to keep the result.
+
+    first, we check the StringBuilder object's length, if the length equal the char array size,
+    we cover the StringBuilder object to string and add it to the result list.
+
+    use a for loop to go through all character in the char array
+    there are 3 point we should check first,
+    first, current character be use or not if true we need directly to go next character
+    then check prev character equal current character and prev character be visited already,
+    if true we need directly to go next character
+    then check the stringBuilder length, if length equal to the index of the character we need to go to next character
+    because we dont want the character be in the origin place
+
+    after check this, we set the current character be used, and append to the StringBuild object,
+    recursive use the function
+    then delete the last character from the stringBuilder object
+    set current character not be used.
+
+     */
 }
