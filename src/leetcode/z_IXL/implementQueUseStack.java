3c0a130fd15b8439834864f3b26fea969c022dec
qifu <fuqiqi12345@gmail.com>
2017-04-27 11:20:31 -0400

    leetcode

--- src/leetcode/z_IXL/implementQueUseStack.java
+++ src/leetcode/z_IXL/implementQueUseStack.java
@@ -1,5 +1,7 @@
 package leetcode.z_IXL;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -48,4 +50,36 @@ public class implementQueUseStack {
 
 
      */
+
+    //用queue实现stack
+    private Queue queue;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        Queue q = new LinkedList<>();
+        q.add(x);
+        q.add(queue);
+        queue = q;
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        int cur = (int) queue.poll();
+        queue = (Queue) queue.peek();
+        return cur;
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return (int)queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
 }
