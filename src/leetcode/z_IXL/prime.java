07288e9bdb7915faee30a85f19b319c3d8f99c53
qifu <fuqiqi12345@gmail.com>
2017-04-27 11:51:36 -0400

    leetcode

---
+++ src/leetcode/z_IXL/prime.java
@@ -0,0 +1,28 @@
+package leetcode.z_IXL;
+
+/**
+ * Created by qifu on 17/4/27.
+ */
+public class prime {
+    public int[] getPrime(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        res[1] = 1;
+        for(int i = 2; i < Math.sqrt(n); i++) {
+            if(res[i] == 0) {
+                for(int j = i * i ; j < n; j+= i) {
+                    res[i] = 1;
+                }
+            }
+        }
+        return res;
+    }
+    public void output_p(int[] res) {
+        for(int i = 0; i < res.length; i++) {
+            if(res[i] == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
