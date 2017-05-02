af973ec2a06282d1402ca2e9c0bbbdc67f6769d4
qifu <fuqiqi12345@gmail.com>
2017-05-02 12:28:58 -0400

    leetcode397

--- src/leetcode/bit/IntegerReplacement.java
+++ src/leetcode/bit/IntegerReplacement.java
@@ -42,4 +42,29 @@ public class IntegerReplacement {
     //http://blog.csdn.net/yeqiuzs/article/details/52506492
     //每遇到奇数时，分别判断n-1还是n+1的尾部零更多，越多的当然步骤越少。
     //特殊case：当n==Integer 最大值时，当n==3时
+
+    public int integerReplacement(int n) {
+        int count = 0;
+        while(n != 1) {
+            if((n&1) == 0) {
+                n >>>= 1;
+            } else if(n == 3 || ((n>>>1)&1) == 0){
+                --n;
+            } else {
+                ++n;
+            }
+            count++;
+        }
+        return count;
+    }
+    /*
+    改进
+    可以不需要写help函数
+    只要保证 选择 n+1 或者 n-1中  1少的那个  可以用Integer.bitCount
+    也可以直接对当前数字最后两位进行操作来判断
+    如果是 01 ->则进行 -1操作 可得到00 是的1的总数更少
+    如果是 11 ->则进行 +1操作 至少会与-1操作 得到相同数量的1 或者更好
+        11111 +1  => 100000
+        11111 -1  => 11110
+     */
 }
