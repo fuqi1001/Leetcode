1f9dff5fecf99fc7bb63db26589525e82005bb32
qifu <fuqiqi12345@gmail.com>
2017-05-02 14:30:00 -0400

    leetcode55

--- src/leetcode/DPandBackTracking/JumpGame.java
+++ src/leetcode/DPandBackTracking/JumpGame.java
@@ -35,4 +35,29 @@ public class JumpGame {
         }
         return point >= nums.length - 1;
     }
+
+    //邪道?
+    public boolean canJump(int[] nums) {
+        if(nums.length < 2) return true;
+
+        for(int curr = nums.length-2; curr>=0;curr--){
+            if(nums[curr] == 0){
+                int neededJumps = 1;
+                while(neededJumps > nums[curr]){
+                    neededJumps++;
+                    curr--;
+                    if(curr < 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+    跳0.
+    从后往前扫
+    如果碰到0, 则用NeededJumps来寻找当前点之前的符合条件的点来跳过这个0
+    若果一直找不到nums[curr] > needeJumps则说明跳不过这个0  则不存在解
+
+     */
 }
