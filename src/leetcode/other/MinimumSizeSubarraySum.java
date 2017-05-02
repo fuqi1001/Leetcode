529d4818a967b2ee44f1cf9c90150de7752f9a54
qifu <fuqiqi12345@gmail.com>
2017-05-02 11:40:11 -0400

    leetcode209

--- src/leetcode/other/MinimumSizeSubarraySum.java
+++ src/leetcode/other/MinimumSizeSubarraySum.java
@@ -4,29 +4,54 @@ package leetcode.other;
  * Created by qifu on 16/11/11.
  */
 public class MinimumSizeSubarraySum {
-    public int minSubArrayLen(int s, int[] nums){
-        if(nums == null || nums.length == 0) return 0;
+
+    //O(n)
+    public int minSubArrayLen(int s, int[] nums) {
+        int left = 0, right = 0, sum = 0;
+        int res = Integer.MAX_VALUE;
         int len = nums.length;
-        int left =0, right = 0;
-        int sum = 0;
-        int minLen = Integer.MAX_VALUE;
-        while(right < len){
-            if(sum < s){
-                sum += nums[right];
-                right++;
-            }else{
-                minLen =  Math.min(minLen, right - left);
-                if(left == right - 1) return 1;
-                sum -= nums[left];
-                left++;
+        while(right < len) {
+            sum += nums[right++];
+            while(sum >= s) {
+                res = Math.min(res, right - left);
+                sum -= nums[left++];
             }
         }
 
-        while(sum >= s){
-            minLen = Math.min(minLen, right - left);
-            sum -= nums[left];
-            left++;
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    //O(nlogn)
+    /*
+    当不能对数组进行排序, 又需要logn的算法时
+    课考虑进行binary search
+    因为都是正数,对原数组累加就会得到一个排好序的递增数组
+    可进行binary search
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        int[] sum = new int[nums.length + 1];
+        for(int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
         }
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < sum.length; i++) {
+            int end = binarySearch(i + 1, sum.length - 1, sum[i] + s, sum);
+            if(end == sum.length) break;
+            if(end - i < res) {
+                res = end - i;
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+    public int binarySearch(int left, int right, int target, int[] sum) {
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(sum[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
