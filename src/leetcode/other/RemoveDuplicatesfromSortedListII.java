79a3aabe53166c74acdcc2c8a697cfd7ccb161f3
qifu <fuqiqi12345@gmail.com>
2017-05-02 15:13:43 -0400

    leetcode85

--- src/leetcode/other/RemoveDuplicatesfromSortedListII.java
+++ src/leetcode/other/RemoveDuplicatesfromSortedListII.java
@@ -20,4 +20,12 @@ public class RemoveDuplicatesfromSortedListII {
         }
         return dummy.next;
     }
+    /*
+        用head 当做当前点, 只有当跳过所有重复点(多次重复)的时候 head才移动
+        head 只会停在unique 的点上
+        dummy(0) -> 3 -> 3 ->4 -> 4 -> 5
+        head 不会停在3 和 4 上
+        会跳到5上去
+
+     */
 }
