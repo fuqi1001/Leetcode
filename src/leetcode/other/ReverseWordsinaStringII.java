3f18167768c7862933fba8c2cb166652e55ad684
qifu <fuqiqi12345@gmail.com>
2017-05-26 02:47:31 -0400

    leetcode186

--- src/leetcode/other/ReverseWordsinaStringII.java
+++ src/leetcode/other/ReverseWordsinaStringII.java
@@ -4,25 +4,28 @@ package leetcode.other;
  * Created by qifu on 17/2/8.
  */
 public class ReverseWordsinaStringII {
-    private void reverse(char[] s, int start, int end){
-        while(start < end){
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
-        }
-    }
     public void reverseWords(char[] s) {
-        reverse(s, 0, s.length - 1);
-
+        int len = s.length - 1;
+        reverse(s, 0, len);
         int start = 0;
-        for(int i = 0; i < s.length; i++){
-            if(s[i] == ' '){
+        for(int i = 0; i <= len; i++) {
+            if(s[i] == ' ') {
                 reverse(s, start, i - 1);
                 start = i + 1;
             }
         }
-        reverse(s, start, s.length - 1);
+        reverse(s, start, len);
     }
+    public void reverse(char[] s, int start, int end) {
+        while(start < end) {
+            char temp = s[start];
+            s[start] = s[end] ;
+            s[end] =  temp;
+            start++;
+            end--;
+        }
+    }
+    /*
+    先整个翻转,再对每个词进行翻转
+     */
 }
