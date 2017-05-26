e1725a2dc39b7803258f0931306114dc18d8950a
qifu <fuqiqi12345@gmail.com>
2017-05-26 02:06:02 -0400

    leetcode148

--- src/leetcode/other/SortList.java
+++ src/leetcode/other/SortList.java
@@ -6,50 +6,47 @@ package leetcode.other;
 public class SortList {
 
     //merge sort
-
-    private ListNode findMiddle(ListNode head){
+    /*
+    每次递归都找中点然后分左右两个部分分别处理子部分
+    然后把字部分合并到一起,较小的append到当前list上然后移动指针
+    直到左或右部分为空,再把剩下的部分append到list上
+     */
+    private ListNode findMid(ListNode head) {
         ListNode fast = head.next;
         ListNode slow = head;
-
-        while(fast!=null && fast.next != null){
+        while(fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
         return slow;
     }
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode middle = findMid(head);
+        ListNode right = sortList(middle.next);
+        middle.next = null;
+        ListNode left = sortList(head);
+        return merge(left, right);
+    }
 
-    private ListNode merge(ListNode head1, ListNode head2){
+    private ListNode merge(ListNode left, ListNode right) {
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-
-        while(head1 != null && head2 != null){
-            if(head1.val < head2.val){
-                tail.next = head1;
-                head1 = head1.next;
-            }else{
-                tail.next = head2;
-                head2 = head2.next;
+        while(left != null && right != null) {
+            if(left.val > right.val) {
+                tail.next = right;
+                right = right.next;
+            } else {
+                tail.next = left;
+                left = left.next;
             }
             tail = tail.next;
         }
-
-        if(head1 != null){
-            tail.next = head1;
-        }else{
-            tail.next = head2;
+        if(left!= null) {
+            tail.next = left;
+        } else {
+            tail.next = right;
         }
-
         return dummy.next;
-
-    }
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode mid = findMiddle(head);
-
-        ListNode right = sortList(mid.next);
-        mid.next = null;
-        ListNode left = sortList(head);
-
-        return merge(left,right);
     }
 }
