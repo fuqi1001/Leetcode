645f75996e056da71e3e84ef2b14b20cbfaf9c75
qifu <fuqiqi12345@gmail.com>
2017-05-27 14:16:32 -0400

    leetcode322

--- src/leetcode/DPandBackTracking/CoinChange.java
+++ src/leetcode/DPandBackTracking/CoinChange.java
@@ -6,6 +6,21 @@ import java.util.Arrays;
  * Created by qifu on 16/11/20.
  */
 public class CoinChange {
+
+    //
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for(int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int coin: coins) {
+            for(int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] == Integer.MAX_VALUE ?Integer.MAX_VALUE : dp[i-coin]+ 1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+    //
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         for(int i = 1 ; i <= amount ; i++){
@@ -35,3 +50,7 @@ public class CoinChange {
     }
 }
 
+/*
+0x7fffffff ==> 首位符号位为0 余下为1
+0x7fff_fffe  除末尾为0, 余下全为1
+ */
