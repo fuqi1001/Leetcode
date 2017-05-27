51c59165d2c2b97b02a5e66decbadf22917508bf
qifu <fuqiqi12345@gmail.com>
2017-05-27 10:06:13 -0400

    leetcode210

--- src/leetcode/other/CourseSscheduleII.java
+++ src/leetcode/other/CourseSscheduleII.java
@@ -9,35 +9,41 @@ import java.util.Queue;
 public class CourseSscheduleII {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-
-        for(int[] cur : prerequisites){
+        for(int[] cur : prerequisites) {
             indegree[cur[0]]++;
         }
+
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0){
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
                 queue.add(i);
             }
         }
         int[] res = new int[numCourses];
         int index = 0;
-        while(!queue.isEmpty()){
+        while(!queue.isEmpty()) {
             numCourses--;
-            int cur_course = queue.poll();
-            res[index++] = cur_course;
-
-            for(int[] pair : prerequisites){
-                if(pair[1] == cur_course){
-                    indegree[pair[0]]--;
-                    if(indegree[pair[0]] == 0){
-                        queue.add(pair[0]);
+            int finished = queue.poll();
+            res[index++] = finished;
+            for(int[] cur : prerequisites) {
+                if(cur[1] == finished) {
+                    indegree[cur[0]]--;
+                    if(indegree[cur[0]] == 0) {
+                        queue.add(cur[0]);
                     }
                 }
             }
         }
         if(numCourses > 0) return new int[0];
-        else {
-            return res;
-        }
+        else return res;
     }
 }
+/*
+开一个数组存入度, 入度为0表示该课程可被完成,
+首先将入度为0的课程存入队列,
+扫队列, 将以当前课程为前置的课程入度减1,
+如果入度为0 存入队列
+直到队列为空
+若numCourses大于0则表示课程并没全部完成
+
+ */
