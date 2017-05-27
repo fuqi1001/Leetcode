586e00ac9eb1486c646f3a486c4717d7ad3e2109
qifu <fuqiqi12345@gmail.com>
2017-05-27 12:21:02 -0400

    leetcode43

--- src/leetcode/other/MultiplyStrings.java
+++ src/leetcode/other/MultiplyStrings.java
@@ -3,32 +3,31 @@ package leetcode.other;
 /**
  * Created by qifu on 17/2/10.
  */
-public class MultiplyStrings {
+public class iMultiplyStrings {
     public String multiply(String num1, String num2) {
         int m = num1.length();
         int n = num2.length();
-
-        int[] nums = new int[m + n];
-
-        for(int i = m - 1; i >= 0; i--){
-            for(int j = n - 1; j >= 0; j--){
-                int mult = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+        int[] number = new int[m+n];
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
                 int pos1 = i + j;
                 int pos2 = i + j + 1;
-
-                int sum = nums[pos2] + mult;
-
-                nums[pos1] += sum / 10;
-                nums[pos2] = sum % 10;
+                int sum = mul + number[pos2];
+                number[pos1] += sum / 10;
+                number[pos2] = sum % 10;
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for(int cur : nums){
-            if(!(cur == 0 && sb.length() == 0)){
+        for(int cur : number) {
+            if(!(cur == 0 && sb.length() == 0)) {
                 sb.append(cur);
             }
         }
-        return sb.length() == 0? "0" : sb.toString();
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
+/*
+    key point:
+    num1中第i位与num2中第j位相乘, 数字会出现在第i+j与第i+j+1位上
+     */
