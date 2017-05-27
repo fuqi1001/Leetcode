981a7624d455ac12e1ebfdec468bdbde64401390
qifu <fuqiqi12345@gmail.com>
2017-05-27 10:53:52 -0400

    leetcode93

--- src/leetcode/DPandBackTracking/RestoreIPAddresses.java
+++ src/leetcode/DPandBackTracking/RestoreIPAddresses.java
@@ -8,44 +8,36 @@ import java.util.List;
  */
 public class RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
-        ArrayList<String> res = new ArrayList<>();
-        ArrayList<String> list = new ArrayList<>();
-        if(s.length() < 4 || s.length() > 12){
-            return res;
-        }
-        helper(res, list, s, 0);
+        List<String> res = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        if(s.length() < 4 || s.length() > 12) return res;
+        generate(res, list, s, 0);
         return res;
     }
-    private void helper(ArrayList<String> res, ArrayList<String> list, String s, int start){
+    public void generate(List<String> res, List<String> list, String s, int start) {
         if(list.size() == 4) {
-            if(start != s.length()){
-                return;
-            }
+            if(start != s.length()) return;
 
             StringBuilder sb = new StringBuilder();
-            for(String cur : list){
-                sb.append(cur + ".");
-
+            for(String cur : list) {
+                sb.append(cur+".");
             }
             sb.deleteCharAt(sb.length() - 1);
             res.add(sb.toString());
             return;
         }
-
-        for(int i = start; i < s.length() && i < start + 3; i++){
-            String temp = s.substring(start, i + 1);
-            if(isValid(temp)){
-                list.add(temp);
-                helper(res, list, s, i + 1);
+        for(int i = start; i < s.length() && i < start + 3; i++) {
+            String cur = s.substring(start, i+1);
+            if(isValid(cur)) {
+                list.add(cur);
+                generate(res, list, s, i + 1);
                 list.remove(list.size() - 1);
             }
         }
     }
-    private boolean isValid(String num){
-        if(num.charAt(0) == '0'){
-            return num.equals("0");
-        }
-        int digit = Integer.parseInt(num);
-        return digit >= 0 && digit <= 255;
+    public boolean isValid(String num) {
+        if(num.charAt(0) == '0') return num.equals("0");
+        int number = Integer.parseInt(num);
+        return (number >= 0 && number <= 255);
     }
 }
