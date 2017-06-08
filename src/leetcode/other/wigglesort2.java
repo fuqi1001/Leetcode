89c931a61896e40d073668461c9c90f032e3f711
qifu <fuqiqi12345@gmail.com>
2017-06-07 23:40:46 -0400

    leetcode324

--- src/leetcode/other/wigglesort2.java
+++ src/leetcode/other/wigglesort2.java
@@ -8,16 +8,14 @@ import java.util.Arrays;
 public class wigglesort2 {
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
-
-        int mid = (nums.length + 1) >> 1;
+        int mid = (nums.length + 1) / 2;
         int[] temp = new int[nums.length];
-
-        int s = mid ;
-        int t = nums.length;
-        for(int i = 0 ; i < nums.length; i++){
-            temp[i] = (i & 1) == 0 ? nums[--s] : nums[--t];
+        int left = mid;
+        int right = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            temp[i] = (i % 2 == 0) ? nums[--left] : nums[--right];
         }
-        for(int i = 0 ; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             nums[i] = temp[i];
         }
     }
