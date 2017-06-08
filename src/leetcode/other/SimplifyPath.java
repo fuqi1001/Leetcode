e4e31a9e9ef3dd352d939d96ff01a0d3ca2f660b
qifu <fuqiqi12345@gmail.com>
2017-06-08 14:44:45 -0400

    leetcode71

--- src/leetcode/other/SimplifyPath.java
+++ src/leetcode/other/SimplifyPath.java
@@ -6,6 +6,7 @@ import java.util.*;
  * Created by qifu on 17/2/14.
  */
 public class SimplifyPath {
+    //deque
     public String simplifyPath(String path) {
         Deque<String> stack = new LinkedList<>();
         Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
@@ -18,6 +19,25 @@ public class SimplifyPath {
         String res = "";
         for(String cur : stack){
             res = "/" + cur + res;
+        }
+        return res.isEmpty() ? "/" : res;
+    }
+
+    //stack
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..", ".",""));
+        for(String cur : path.split("/")) {
+            if(cur.equals("..") && !stack.isEmpty()) {
+                stack.pop();
+            } else if(!skip.contains(cur)) {
+                stack.push(cur);
+            }
+        }
+
+        String res = "";
+        while(!stack.isEmpty()) {
+            res = "/" + stack.pop() + res;
         }
         return res.isEmpty() ? "/" : res;
     }
