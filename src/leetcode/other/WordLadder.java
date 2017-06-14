3125e612a7a30a4c088e9a23aeb4dff42a552ff0
qifu <fuqiqi12345@gmail.com>
2017-06-13 23:52:50 -0400

    leetcode127

--- src/leetcode/other/WordLadder.java
+++ src/leetcode/other/WordLadder.java
@@ -8,10 +8,9 @@ import java.util.Set;
  * Created by qifu on 17/2/16.
  */
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList){
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(!wordList.contains(endWord)) return 0;
-
-        Set<String> wordSet = new HashSet<String>(wordList);
+        Set<String> wordSet = new HashSet<>(wordList);
         Set<String> start = new HashSet<>();
         Set<String> end = new HashSet<>();
 
@@ -19,36 +18,34 @@ public class WordLadder {
         end.add(endWord);
 
         int res = 1;
-        wordSet.remove(beginWord);
-        wordSet.remove(endWord);
+        wordList.remove(beginWord);
+        wordList.remove(endWord);
 
-
-        while(!start.isEmpty()){
-            if(start.size() > end.size()){
+        while(!start.isEmpty()) {
+            if(start.size() > end.size()) {
                 Set<String> temp = start;
                 start = end;
                 end = temp;
             }
-            Set<String> next = new HashSet<>();
 
-            for(String word : start){
-                char[] strArray = word.toCharArray();
-                for(int i = 0; i < strArray.length; i++){
-                    char old = strArray[i];
-                    for(char c = 'a'; c <= 'z'; c++){
-                        strArray[i] = c;
-                        String newStr = String.valueOf(strArray);
-                        if(end.contains(newStr)){
-                            return res + 1;
-                        }
-                        if(wordSet.contains(newStr)){
-                            next.add(newStr);
-                            wordSet.remove(newStr);
+            Set<String> next = new HashSet<>();
+            for(String word : start) {
+                char[] _word = word.toCharArray();
+                for(int i = 0; i < _word.length; i++) {
+                    char prev = _word[i];
+                    for(char c = 'a' ; c <= 'z'; c++) {
+                        _word[i] = c;
+                        String new_word = String.valueOf(_word);
+                        if(end.contains(new_word)) return res + 1;
+                        if(wordSet.contains(new_word)) {
+                            wordSet.remove(new_word);
+                            next.add(new_word);
                         }
                     }
-                    strArray[i] = old;
+                    _word[i] = prev;
                 }
             }
+
             start = next;
             res++;
         }
