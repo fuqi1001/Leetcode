60e75b54e058f3c85e5f5de7f2fdac367be84605
qifu <fuqiqi12345@gmail.com>
2017-06-14 00:02:46 -0400

    leetcode127

--- src/leetcode/other/WordLadder.java
+++ src/leetcode/other/WordLadder.java
@@ -14,16 +14,16 @@ public class WordLadder {
         Set<String> start = new HashSet<>();
         Set<String> end = new HashSet<>();
 
+        int res = 1;
+        wordSet.remove(beginWord);
+        wordSet.remove(endWord);
         start.add(beginWord);
         end.add(endWord);
 
-        int res = 1;
-        wordList.remove(beginWord);
-        wordList.remove(endWord);
-
         while(!start.isEmpty()) {
             if(start.size() > end.size()) {
-                Set<String> temp = start;
+                Set<String> temp = new HashSet<>();
+                temp = start;
                 start = end;
                 end = temp;
             }
@@ -33,22 +33,30 @@ public class WordLadder {
                 char[] _word = word.toCharArray();
                 for(int i = 0; i < _word.length; i++) {
                     char prev = _word[i];
-                    for(char c = 'a' ; c <= 'z'; c++) {
+                    for(char c = 'a'; c <= 'z'; c++) {
                         _word[i] = c;
-                        String new_word = String.valueOf(_word);
-                        if(end.contains(new_word)) return res + 1;
-                        if(wordSet.contains(new_word)) {
-                            wordSet.remove(new_word);
-                            next.add(new_word);
+                        String _new = String.valueOf(_word);
+                        if(end.contains(_new)) return res+1;
+                        if(wordSet.contains(_new)) {
+                            wordSet.remove(_new);
+                            next.add(_new);
                         }
                     }
                     _word[i] = prev;
                 }
             }
-
             start = next;
             res++;
         }
         return 0;
     }
+
+    /*
+    创建三个SET 分别存词典, start词, end词
+    对start词中进行遍历,寻找改变单个字母课变为wordset中词的 string
+    将其从wordSet中remove 放入next中
+    在循环最后将next覆给start
+
+    start 与 end set的交换是为了竟可能少的遍历
+     */
 }
