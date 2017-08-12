48f9e2699ab8d157e8012824ffa832a2d5c1e3e4
qifu <fuqiqi12345@gmail.com>
2017-08-12 18:32:04 -0400

    leetcode535

--- src/leetcode/Implement/EncodeandDecodeTinyURL.java
+++ src/leetcode/Implement/EncodeandDecodeTinyURL.java
@@ -1,7 +1,9 @@
 package leetcode.Implement;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qifu on 17/3/15.
@@ -18,5 +20,35 @@ public class EncodeandDecodeTinyURL {
     public String decode(String shortUrl) {
         int index = Integer.valueOf(shortUrl);
         return (index < list.size()) ? list.get(index) : "";
+    }
+
+    //Other
+    //It's possible that a randomly generated code has already been generated before.
+    //In that case, another random code is generated instead.
+
+    Map<String, String> index = new HashMap<>();
+    Map<String, String> reIndex = new HashMap<>();
+    static String BASE_HOST = "http://tinyurl.com/";
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        if(reIndex.containsKey(longUrl)) return BASE_HOST + reIndex.get(longUrl);
+        String charStr =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String str = null;
+        do {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 6; i++) {
+                int randIndex = (int)(Math.random() * charStr.length());
+                sb.append(charStr.charAt(randIndex));
+            }
+            str = sb.toString();
+        } while(index.containsKey(str));
+        index.put(longUrl, str);
+        reIndex.put(str, longUrl);
+        return BASE_HOST + str;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return reIndex.get(shortUrl.replace(BASE_HOST, ""));
     }
 }
