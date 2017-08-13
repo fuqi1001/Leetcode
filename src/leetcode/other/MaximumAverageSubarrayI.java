f512388a1714cea755e62a6cacf0153b2555c154
qifu <fuqiqi12345@gmail.com>
2017-08-13 15:53:04 -0400

    leetcode643

---
+++ src/leetcode/other/MaximumAverageSubarrayI.java
@@ -0,0 +1,19 @@
+package leetcode.other;
+
+/**
+ * Created by qifu on 17/8/13.
+ */
+public class MaximumAverageSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+        long sum = 0;
+        for (int i = 0; i < k; i++) sum += nums[i];
+        long max = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+
+        return max / 1.0 / k;
+    }
+}
