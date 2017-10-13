e93aef9d9c4ef763cc5b573f0e02a2472174e7b1
qifu <fuqiqi12345@gmail.com>
2017-10-12 22:30:34 -0400

    leetcode 498

--- src/leetcode/other/DiagonalTraverse.java
+++ src/leetcode/other/DiagonalTraverse.java
@@ -6,18 +6,13 @@ package leetcode.other;
 public class DiagonalTraverse {
     public int[] findDiagonalOrder(int[][] matrix) {
         if(matrix == null || matrix.length == 0) return new int[0];
-        int m = matrix.length;
-        int n = matrix[0].length;
-
+        int m = matrix.length, n = matrix[0].length;
         int[][] dir = {{-1, 1}, {1, -1}};
-        int row = 0;
-        int col = 0;
+        int row = 0, col = 0;
+        int[] res = new int[m * n];
         int d = 0;
-
-        int[] result = new int[m * n];
         for(int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-
+            res[i] = matrix[row][col];
             row += dir[d][0];
             col += dir[d][1];
 
@@ -25,9 +20,8 @@ public class DiagonalTraverse {
             if(col >= n) {col = n - 1; row += 2; d = 1 - d;}
             if(row < 0) {row = 0; d = 1 - d;}
             if(col < 0) {col = 0; d = 1 - d;}
-
         }
-        return result;
+        return res;
     }
     /*
     If out of bottom border (row >= m) then row = m - 1; col += 2; change walk direction.
