87dd9e634cfcbad9288fe297aeec5c15fb75a9f6
qifu <fuqiqi12345@gmail.com>
2017-10-12 21:42:20 -0400

    leetcode 88

--- src/leetcode/other/MergeSortedArray.java
+++ src/leetcode/other/MergeSortedArray.java
@@ -5,23 +5,15 @@ package leetcode.other;
  */
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n){
-
-        int index = m + n - 1;
-        int i = m - 1;
-        int j = n - 1;
-        while(i >=0 && j >= 0){
-            if(nums1[i] > nums2[j]){
-                nums1[index--] = nums1[i--];
-            }
-            else{
-                nums1[index--] = nums2[j--];
-            }
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while(i > -1 && j > -1) {
+            nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
         }
-        while(i >= 0){
-            nums1[index--] = nums1[i--];
-        }
-        while(j >= 0){
-            nums1[index--] = nums2[j--];
+        while(j > -1) {
+            nums1[k--] = nums2[j--];
         }
     }//从后向前填空
+
+
+
 }
