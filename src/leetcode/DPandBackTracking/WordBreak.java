d7034d84594ad495196a55c157de212b67acf9e4
qifu <fuqiqi12345@gmail.com>
2017-10-16 22:38:56 -0400

    leetcode 139

--- src/leetcode/DPandBackTracking/WordBreak.java
+++ src/leetcode/DPandBackTracking/WordBreak.java
@@ -1,34 +1,32 @@
 package leetcode.DPandBackTracking;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by qifu on 16/11/18.
  */
 public class WordBreak {
-    public boolean wordBreak(String s, Set<String> wordDict) {
-        Set<Integer> set = new HashSet<>();
-        return dfs(s,0,wordDict,set);
-
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<Integer> notValid = new HashSet<>();
+        return helper(s, wordDict, notValid, 0);
     }
-
-    private boolean dfs(String s, int index, Set<String> dict, Set<Integer> set){
+    public boolean helper(String s, List<String> wordDict, Set<Integer> notValid, int index){
         if(index == s.length()) return true;
-        if(set.contains(index)) return false;
+        if(notValid.contains(index)) return false;
 
-        for(int i = index+1; i <= s.length() ; i++){
+        for(int i = index + 1; i < s.length() + 1; i++){
             String temp = s.substring(index, i);
-            if(dict.contains(temp)){
-                if(dfs(s,i,dict,set)){
+            if(wordDict.contains(temp)){
+                if(helper(s, wordDict, notValid, i)){
                     return true;
-                }else{
-                    set.add(i);
+                } else {
+                    notValid.add(i);
                 }
             }
         }
-
-        set.add(index);
+        notValid.add(index);
         return false;
     }
 }
