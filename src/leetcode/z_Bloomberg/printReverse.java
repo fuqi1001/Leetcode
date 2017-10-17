95cacb4e57f6f21485e9074c6a42d4e3e1581da9
qifu <fuqiqi12345@gmail.com>
2017-10-16 22:06:17 -0400

    reverse print list node

---
+++ src/leetcode/z_Bloomberg/printReverse.java
@@ -0,0 +1,36 @@
+package leetcode.z_Bloomberg;
+
+public class printReverse {
+    static Node head = new Node(0);
+    static Node cur = head;
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            this.data = d;
+            next = null;
+        }
+    }
+    public void push(int num) {
+        Node new_node = new Node(num);
+        cur.next = new_node;
+        cur = new_node;
+    }
+    public void print(Node head) {
+        if(head == null) return;
+        print(head.next);
+        System.out.println(head.data);
+    }
+    public static void main(String[] args) {
+        printReverse t = new printReverse();
+        t.push(1);
+        t.push(2);
+        t.push(3);
+        t.push(4);
+        t.push(5);
+        t.push(6);
+        t.print(head);
+
+    }
+}
