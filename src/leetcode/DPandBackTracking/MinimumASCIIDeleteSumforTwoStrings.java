93ae5f6f494fdb5683fe745c9f5a371a74f00823
qifu <fuqiqi12345@gmail.com>
2017-10-22 14:27:49 -0400

    leetcode 712

---
+++ src/leetcode/DPandBackTracking/MinimumASCIIDeleteSumforTwoStrings.java
@@ -0,0 +1,22 @@
+package leetcode.DPandBackTracking;
+
+public class MinimumASCIIDeleteSumforTwoStrings {
+    public int minimumDeleteSum(String s1, String s2) {
+        int[][] count = new int[s1.length()+1][s2.length()+1];
+        for(int i = 1; i < count.length; i++){
+            count[i][0] = count[i-1][0] + s1.charAt(i-1);
+        }
+        for(int j = 1; j < count[0].length; j++) {
+            count[0][j] = count[0][j-1] + s2.charAt(j-1);
+        }
+
+        for(int i = 1; i < count.length; i++){
+            for(int j = 1; j < count[0].length; j++) {
+                int cost = (s1.charAt(i-1) == s2.charAt(j-1)) ? 0 : s1.charAt(i-1)+s2.charAt(j-1);
+                count[i][j] = Math.min(count[i-1][j] + s1.charAt(i-1), count[i][j-1]+s2.charAt(j-1));
+                count[i][j] = Math.min(count[i][j], count[i-1][j-1] + cost);
+            }
+        }
+        return count[s1.length()][s2.length()];
+    }
+}
