d5b37c6f8f0dd199bc85b4dcdc1b05b6c1747381
qifu <fuqiqi12345@gmail.com>
2017-10-24 20:44:01 -0400

    leetcode 387

--- src/leetcode/other/FirstUniqueCharacter.java
+++ src/leetcode/other/FirstUniqueCharacter.java
@@ -1,5 +1,7 @@
 package leetcode.other;
 
+import java.util.*;
+
 /**
  * Created by qifu on 16/8/29.
  */
@@ -14,5 +16,51 @@ public class FirstUniqueCharacter {
             if(count[s.charAt(i) - 'a'] == 1) return i;
         }
         return -1;
+    }
+
+
+    ///
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new LinkedHashMap<>();
+        Set<Character> set = new HashSet<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(set.contains(s.charAt(i))){
+                map.remove(s.charAt(i));
+            } else {
+                set.add(s.charAt(i));
+                map.put(s.charAt(i), i);
+            }
+        }
+
+        if(map.size() == 0) return -1;
+        int res = 0;
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            res = entry.getValue();
+            break;
+        }
+        return res;
+    }
+
+    //
+    public int firstUniqChar(String s) {
+        if(s == null || s.length() == 0) return -1;
+        int len = s.length();
+        if(len == 1) return 0;
+        int slow = 0, fast = 1;
+        char[] ctr = s.toCharArray();
+        int[] count = new int[256];
+        count[ctr[slow]]++;
+        while(fast < len) {
+            count[ctr[fast]]++;
+            while( slow < len && count[ctr[slow]] > 1) slow++;
+            if(slow >= len)  return -1;
+            if(count[ctr[slow]] == 0) {
+                count[ctr[slow]]++;
+                fast = slow;
+            }
+            fast++;
+        }
+        return slow;
     }
 }
