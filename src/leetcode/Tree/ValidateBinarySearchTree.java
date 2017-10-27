bed24cc13a3af29638f5804166a4aba9bcf42466
qifu <fuqiqi12345@gmail.com>
2017-10-26 23:36:24 -0400

    leetcode 98

--- src/leetcode/Tree/ValidateBinarySearchTree.java
+++ src/leetcode/Tree/ValidateBinarySearchTree.java
@@ -1,5 +1,7 @@
 package leetcode.Tree;
 
+import java.util.Stack;
+
 /**
  * Created by qifu on 17/2/15.
  */
@@ -24,5 +26,23 @@ public class ValidateBinarySearchTree {
         if(min != null && min >= root.val) return false;
         if(max != null && max <= root.val) return false;
         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+
+    //
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while(root != null || !stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
 }
