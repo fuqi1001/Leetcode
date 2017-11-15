366cb382521dad8b2705d1b61a4cd40e67f9a212
qifu <fuqiqi12345@gmail.com>
2017-11-15 11:24:16 -0500

    leetcode215

--- src/leetcode/other/KthLargestElementinanArray.java
+++ src/leetcode/other/KthLargestElementinanArray.java
@@ -8,39 +8,30 @@ public class KthLargestElementinanArray {
     //quick select
     public int findKthLargest(int[] nums, int k) {
         if(k < 1 || nums == null) return 0;
-        return getKth(nums.length - (k - 1) ,nums,0,nums.length-1);
+        return helpFindK(nums.length - (k - 1), nums, 0, nums.length - 1);
     }
-    public int getKth(int k, int[] nums, int start, int end){
-        int pivot = nums[end];
-        int left = start ;
-        int right = end;
 
-        while(true){
-            while(nums[left] < pivot && left < right){
-                left++;
-            }
-            while(nums[right] >= pivot && right > left){
-                right--;
-            }
+    private int helpFindK(int k, int[] nums, int start, int end) {
+        int pivot = nums[end], left = start, right = end;
+        while(true) {
+            while(nums[left] < pivot && left < right) left++;
+            while(nums[right] >= pivot && left < right) right--;
             if(left == right) break;
-
-            swap(nums,left,right);
+            swap(nums, left, right);
         }
-        swap(nums,left,end);
-
-        if(k == left + 1){
-            return return pivot;
-        } else if (k < left + 1){
-            return getKth(k,nums,start,left - 1);
-        }else{
-            return getKth(k,nums,left + 1,end);
+        swap(nums,left, end);
+        if(k == left + 1) {
+            return pivot;
+        } else if(k < left + 1) {
+            return helpFindK(k, nums, start, left - 1);
+        } else {
+            return helpFindK(k, nums, left + 1, end);
         }
     }
-
-    private void swap(int[] nums,int n1, int n2){
-        int tmp = nums[n1];
-        nums[n1] = nums[n2];
-        nums[n2] = tmp;
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 
 
