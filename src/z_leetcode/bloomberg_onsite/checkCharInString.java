32437da4ca0d0face7f21c661538479d737ae64e
qifu <fuqiqi12345@gmail.com>
2017-11-16 15:00:24 -0500

    输入一个string 一个char 然后让你找这个string里面所有字母到这个string里含有的这个char的位置的最近的距离

---
+++ src/z_leetcode/bloomberg_onsite/checkCharInString.java
@@ -0,0 +1,39 @@
+package z_leetcode.bloomberg_onsite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class checkCharInString {
+    public static int[] check(String s, char c) {
+        int[] res = new int[s.length()];
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == c) {
+                list.add(i);
+            }
+        }
+
+        if(list.size() == 0) return res;
+        int index = 0;
+        int cur = list.get(index);
+        if(list.size() == 1) {
+            for(int i = 0; i < s.length(); i++) {
+                res[i] = Math.abs(cur - i);
+            }
+        } else {
+            int next = cur;
+            for(int i = 0; i < s.length(); i++) {
+                if(index + 1 < list.size()) {
+                    next = list.get(++index);
+                }
+                if(Math.abs(cur - i) <= Math.abs(next - i)) {
+                    res[i] = Math.abs(cur - i);
+                } else {
+                    cur = next;
+                    res[i] = Math.abs(cur - i);
+                }
+            }
+        }
+        return res;
+    }
+}
