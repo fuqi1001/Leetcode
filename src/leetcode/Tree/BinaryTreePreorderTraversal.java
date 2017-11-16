cd8c900364edd145b939fe9220880021d7552ff7
qifu <fuqiqi12345@gmail.com>
2017-11-16 10:52:04 -0500

    leetcode 144

--- src/leetcode/Tree/BinaryTreePreorderTraversal.java
+++ src/leetcode/Tree/BinaryTreePreorderTraversal.java
@@ -38,4 +38,19 @@ public class BinaryTreePreorderTraversal {
         }
         return result;
     }
+
+    //
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(root == null) return list;
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if(node.right != null) stack.push(node.right);
+            if(node.left != null) stack.push(node.left);
+        }
+        return list;
+    }
 }
