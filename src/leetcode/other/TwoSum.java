53bc53d8f5d3d59240003b6eee5840db37063fce
qifu <fuqiqi12345@gmail.com>
2017-12-06 23:05:23 -0500

    leetcode

--- src/leetcode/other/TwoSum.java
+++ src/leetcode/other/TwoSum.java
@@ -29,15 +29,9 @@ public class TwoSum {
         int max = nums[0];
         int min = nums[0];
 
-        for(int i = 0 ; i < nums.length; i++){
-            if(nums[i] > max){
-                max = nums[i];
-            }
-        }
-        for(int i = 0 ; i < nums.length; i++){
-            if(nums[i] < min){
-                min = nums[i];
-            }
+        for(int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
         }
 
         int Min =  Math.min(min,target - max);
