424a3acaa5beccf2fec2e8e4e01a39b23d2b36eb
qifu <fuqiqi12345@gmail.com>
2017-12-06 20:51:03 -0500

    leetcode

--- src/leetcode/other/IslandPerimeter.java
+++ src/leetcode/other/IslandPerimeter.java
@@ -33,4 +33,32 @@ public class IslandPerimeter {
         }
         return res;
     }
+
+
+    //DFS
+    public int islandPerimeter(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) return helper(grid, i, j);
+            }
+        }
+        return 0;
+    }
+
+    public int helper(int[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return 1;
+        if(grid[i][j] == 0) return 1;
+        if(grid[i][j] == -1) return 0;
+
+        int count = 0;
+        grid[i][j] = -1;
+
+        count += helper(grid, i+1, j);
+        count += helper(grid, i-1, j);
+        count += helper(grid, i, j + 1);
+        count += helper(grid, i, j - 1);
+
+        return count;
+    }
 }
