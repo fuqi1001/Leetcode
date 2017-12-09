aef81f59fbcd5aae288e6a43cf9bbe4dcb7d44e6
qifu <fuqiqi12345@gmail.com>
2017-12-09 18:01:46 -0500

    leetcode

--- src/leetcode/other/StringCompression.java
+++ src/leetcode/other/StringCompression.java
@@ -2,21 +2,20 @@ package leetcode.other;
 
 public class StringCompression {
     public int compress(char[] chars) {
-        int len = chars.length, count = 1, index = 0;
+        int index = 0, len = chars.length, count = 1;
         for(int i = 1; i <= len; i++) {
-            if(i < len && chars[i] == chars[i - 1]){
+            if(i < len && chars[i] == chars[i - 1]) {
                 count++;
                 continue;
             }
             chars[index++] = chars[i - 1];
             if(count != 1) {
-                String s = Integer.toString(count);
-                for(int k = 0; k < s.length(); k++) {
-                    chars[index++] = s.charAt(k);
+                String num = String.valueOf(count);
+                for(char c : num.toCharArray()) {
+                    chars[index++] = c;
                 }
             }
-            count = 0;
-            count++;
+            count = 1;
         }
         return index;
     }
