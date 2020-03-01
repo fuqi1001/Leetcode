7030afb7e5e77ab8cbc65b94023066db8224e2e9
fuqi1001 <fuqiqi12345@gmail.com>
2020-02-29 23:55:15 -0500

    update

---
+++ src/leetcode/other/RankTeamsByVotes.java
@@ -0,0 +1,35 @@
+package leetcode.other;
+
+/**
+ * Created by qifu on 20/2/29.
+ */
+
+public class RankTeamsByVotes {
+    public String rankTeams(String[] votes) {
+        Map<Character, int[]> map = new HashMap<>();
+        int len = votes[0].length;
+        for(String vote : votes) {
+            for(int i = 0; i < len; i++) {
+                char c = vote.charAt(i);
+                map.putIfAbsent(c, new int[len]);
+                map.get(c)[i]++;
+            }
+        }
+
+        List<Character> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (a, b) -> {
+            for(int i = 0; i < len; i++) {
+                if (map.get(a)[i] != map.get(b)[i]) {
+                    return map.get(b)[i] - map.get(a)[i];
+                }
+            }
+            return a - b;
+        });
+        StringBuilder sb = new StringBuilder();
+        for(char c: list) {
+            sb.append(c);
+        }
+        return sb.toString();
+
+    }
+}
